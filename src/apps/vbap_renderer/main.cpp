2cedd6c317d8c68ce010e86a27ca9e422cf7843b
Andreas <a.franck@soton.ac.uk>
2014-11-04 17:08:12 +0000

    First functional version of example rendering application (not error-free yet)

--- src/apps/vbap_renderer/main.cpp
+++ src/apps/vbap_renderer/main.cpp
@@ -6,6 +6,7 @@
 
 #include <cstddef>
 #include <cstdlib>
+#include <cstdio> // for getc(), for testing purposes
 #include <iostream>
 
 int main( int argc, char const * const * argv )
@@ -15,12 +16,23 @@ int main( int argc, char const * const * argv )
 
   // define fixed parameters for rendering
   const std::size_t numberOfObjects = 2;
-  const std::size_t numberOfLoudspeakers = 5;
+  const std::size_t numberOfLoudspeakers = 2;
 
   const std::size_t periodSize = 1024;
+  const std::size_t samplingRate = 44100;
 
-  try {
-    rrl::PortaudioInterface audioInterface;
+  try 
+  {
+    rrl::PortaudioInterface::Config interfaceConfig;
+    interfaceConfig.mNumberOfCaptureChannels = numberOfObjects;
+    interfaceConfig.mNumberOfPlaybackChannels = numberOfLoudspeakers;
+    interfaceConfig.mPeriodSize = periodSize;
+    interfaceConfig.mSampleRate = samplingRate;
+    interfaceConfig.mInterleaved = false;
+    interfaceConfig.mSampleFormat = rrl::PortaudioInterface::Config::SampleFormat::float32Bit;
+    interfaceConfig.mHostApi = "default";
+
+    rrl::PortaudioInterface audioInterface( interfaceConfig );
 
     SignalFlow flow;
 
@@ -30,10 +42,22 @@ int main( int argc, char const * const * argv )
 
     flow.setup();
 
-    audioInterface.registerCallback( &ril::AudioSignalFlow::processFunction );
+    audioInterface.registerCallback( &ril::AudioSignalFlow::processFunction, &flow );
 
+    // should there be a separate start() method for the audio interface?
+    audioInterface.start( );
+
+    char c = std::getc( stdin );
+
+    audioInterface.stop( );
+
+
+    // not needed if the graph is activated by the callback method.
     flow.process();
 
+    // there should be a sleep/wait/whatever call to wait for the termination of rendering.
+
+    // Should there be an explicit stop() method for the sound interface?
 
     audioInterface.unregisterCallback( &ril::AudioSignalFlow::processFunction );
   }
--- src/apps/vbap_renderer/signal_flow.cpp
+++ src/apps/vbap_renderer/signal_flow.cpp
@@ -32,27 +32,29 @@ SignalFlow::process()
 /*virtual*/ void 
 SignalFlow::setup()
 {
-  // TODO: implement me!
-
   // Initialise and configure audio components
   mSum.setup( 2, 2 ); // width = 2, numInputs = 2;
 
   // Define and set the width of the input and output vectors of the graph
 
-  // Set up communication area
-  initCommArea( 4, period( ), ril::cVectorAlignmentBytes );
+  // Set up communication area 
+  // the required width of the communication area is determined by the number of capture inputs of the graph plus 
+  // the number of port outputs.
+  // Note: The alignment of the communication area should be fixed to this value with no user options.
+  initCommArea( 4, period( ), ril::cVectorAlignmentSamples );
 
   // connect the ports
-  std::array<std::size_t, 2> const idxList1 = { 0, 1 };
-  assignCommunicationIndices( std::string( "Add" ), std::string( "in0" ), &idxList1[0], 2 );
+  // std::array<std::size_t, 2> const idxList1 = { 0, 1 };
+  assignCommunicationIndices( "Add", "in0", { 0, 1 } );
 
-  std::array<std::size_t, 2> const idxList2{ 1, 0 };
-  assignCommunicationIndices( std::string( "Add" ), std::string( "in1" ), idxList2 );
-  assignCommunicationIndices( std::string( "Add" ), std::string( "in1" ), std::array<std::size_t, 2>{ 1, 0 } );
+  // std::array<std::size_t, 2> const idxList2 = { 1, 0 };
+  assignCommunicationIndices( std::string( "Add" ), std::string( "in1" ), { 1, 0 } );
 
   std::vector<std::size_t> idxList3{ 2, 3 };
   assignCommunicationIndices( std::string( "Add" ), std::string( "out" ), idxList3.begin( ), idxList3.end( ) );
 
+  assignCaptureIndices( {0, 1} );
+  assignPlaybackIndices( {2, 3} );
 
   setInitialised( true );
 }
