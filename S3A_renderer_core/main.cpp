e996da5d3fb515e3851b4c82b7d75f82473543d5
dylan <dylan@dhcp-162-158.wireless.soton.ac.uk>
2014-11-10 15:45:46 +0000

    Initial Commit

---
+++ S3A_renderer_core/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  S3A_renderer_core
+//
+//  Created by dylan on 10/11/2014.
+//  Copyright (c) 2014 ISVR, Southampton University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
