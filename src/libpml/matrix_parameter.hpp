f77ff2948cbc9744eb8507674b11ceabd6f0d814
Andreas <a.franck@soton.ac.uk>
2014-12-06 10:41:10 +0000

    Added two new libraries (first commit, no sensible contents yet)
    * New library libpml: Parameter message library
    * New library librbbl: Renderer building block library

---
+++ src/libpml/matrix_parameter.hpp
@@ -0,0 +1,57 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#ifndef VISR_PML_MATRIX_PARAMETER_HPP_INCLUDED
+#define VISR_PML_MATRIX_PARAMETER_HPP_INCLUDED
+
+#include <libefl/basic_matrix.hpp>
+
+namespace visr
+{
+namespace pml
+{
+
+/**
+ * A type for passing matrixes between processing components.
+ * The template class is explicitly instantiated for the element types float and double.
+ * @tparam ElementType The data type of the elements of the matrix.
+ */
+template<typename ElementType>
+class MatrixParameter
+{
+public:
+  /**
+   * Default constructor, creates an empty matrix of dimension 0 x 0.
+   */
+  MatrixParameter();
+
+  /**
+   * Construct a parameter matrix with the given dimensions.
+   * The matrix is zero-initialized.
+   * @param numRows The number od matrix rows.
+   * @param numColumns The number of columns.
+   */
+  explicit MatrixParameter( std::size_t numRows, std::size_t numColumns );
+
+  /**
+   * Change the matrix dimension.
+   * The content of the matrix is not kept, but reset to zeros.
+   * @param numRows The new number of rows.
+   * @param numColumns The new number of columns
+   * @throw std::bad_alloc If the creation of the new matrix fails.
+   */
+  void resize( std::size_t numRows, std::size_t numColumns );
+
+  std::size_t numberOfColumns() const { return mData.mNumberOfColumns(); }
+
+  std::size_t numberOfRows( ) const { return mData.mNumberOfRows( ); }
+
+
+private:
+  efl::BasicMatrix<ElementType> mData;
+};
+
+} // namespace pml
+} // namespace visr
+
+
+#endif // VISR_PML_MATRIX_PARAMETER_HPP_INCLUDED
