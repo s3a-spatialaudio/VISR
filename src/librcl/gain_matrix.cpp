46972ab998622bddcba01b65711a5e589fc2c5cc
Andreas Franck <a.franck@soton.ac.uk>
2014-12-07 14:25:01 +0000

    Added skeleton of GainMatrix class to librcl
    * No functionality yet.
    * Will use the rbbl::GainMatrix class internally.
    * Also minor documentation improvements to rcl::Add

---
+++ src/librcl/gain_matrix.cpp
@@ -0,0 +1,14 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "gain_matrix.hpp"
+
+#include <libpml/matrix_parameter.hpp>
+
+namespace visr
+{
+namespace rbbl
+{
+
+
+} // namespace rbbl
+} // namespace visr
