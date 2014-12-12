5170519d2829776a34e7ebf570e685d349ff1201
Andreas Franck <a.franck@soton.ac.uk>
2014-12-12 11:42:02 +0000

    Added test application for rendering component GainMatrix.
    * Based on src/mex/gain_matrix Matlab Mex external.

---
+++ src/apps/gain_matrix/main.cpp
@@ -0,0 +1,64 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "signal_flow.hpp"
+
+#include <librrl/portaudio_interface.hpp>
+
+#include <cstddef>
+#include <cstdlib>
+#include <cstdio> // for getc(), for testing purposes
+#include <iostream>
+
+int main( int argc, char const * const * argv )
+{
+  using namespace visr;
+  using namespace visr::apps::gain_matrix;
+
+  // define fixed parameters for rendering
+  const std::size_t numberOfObjects = 2;
+  const std::size_t numberOfLoudspeakers = 2;
+
+  const std::size_t periodSize = 1024;
+  const std::size_t samplingRate = 48000;
+
+  try 
+  {
+    rrl::PortaudioInterface::Config interfaceConfig;
+    interfaceConfig.mNumberOfCaptureChannels = numberOfObjects;
+    interfaceConfig.mNumberOfPlaybackChannels = numberOfLoudspeakers;
+    interfaceConfig.mPeriodSize = periodSize;
+    interfaceConfig.mSampleRate = samplingRate;
+    interfaceConfig.mInterleaved = false;
+    interfaceConfig.mSampleFormat = rrl::PortaudioInterface::Config::SampleFormat::float32Bit;
+    interfaceConfig.mHostApi = "default";
+
+    rrl::PortaudioInterface audioInterface( interfaceConfig );
+
+    const std::size_t cInterpolationLength = 4 * periodSize;
+
+    apps::gain_matrix::SignalFlow flow( numberOfObjects, numberOfLoudspeakers,
+                                       cInterpolationLength, periodSize,
+                                       samplingRate );
+    flow.setup();
+
+    audioInterface.registerCallback( &ril::AudioSignalFlow::processFunction, &flow );
+
+    // should there be a separate start() method for the audio interface?
+    audioInterface.start( );
+
+    // Rendering runs until <Return> is entered on the console.
+    char c = std::getc( stdin );
+
+    audioInterface.stop( );
+
+   // Should there be an explicit stop() method for the sound interface?
+
+    audioInterface.unregisterCallback( &ril::AudioSignalFlow::processFunction );
+  }
+  catch( std::exception const & ex )
+  {
+    std::cout << "Exception caught on top level: " << ex.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
