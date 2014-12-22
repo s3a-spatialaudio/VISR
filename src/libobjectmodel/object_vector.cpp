85b0b89a9c5d6e459a2103b24421c2cf27c4fd30
Andreas Franck <a.franck@soton.ac.uk>
2014-12-22 16:21:23 +0000

    First commit of library libobjectmodel
    * Created skeletons of first classes (nothing working yet).

---
+++ src/libobjectmodel/object_vector.cpp
@@ -0,0 +1,13 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "object_vector.hpp"
+
+namespace visr
+{
+namespace objectmodel
+{
+
+
+
+} // namespace objectmodel
+} // namespace visr
---
+++ src/libobjectmodel/object_vector.hpp
@@ -0,0 +1,27 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#ifndef VISR_OBJECTMODEL_OBJECT_VECTOR_HPP_INCLUDED
+#define VISR_OBJECTMODEL_OBJECT_VECTOR_HPP_INCLUDED
+
+#include <map>
+#include <memory>
+
+namespace visr
+{
+namespace objectmodel
+{
+
+/**
+ *
+ */
+class ObjectVector
+{
+public:
+
+private:
+};
+
+} // namespace objectmodel
+} // namespace visr
+
+#endif // #ifndef VISR_OBJECTMODEL_OBJECT_VECTOR_HPP_INCLUDED
