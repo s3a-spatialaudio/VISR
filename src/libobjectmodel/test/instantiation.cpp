6e6d4084ed88c5a7e105c3d9c47c5e03d7ba00ec
Andreas Franck <a.franck@soton.ac.uk>
2014-12-23 10:43:16 +0000

    Implemented infrastructure for Boost unit tests and implemented first standard auto unit test in libobjectmodel/test
    * Added FIND_PACKAGE( Boost ) to main CMakeLists.txt
    * Implemented BOOST_AUTO_TEST_CASE in in instantiation.cpp

--- src/libobjectmodel/test/instantiation.cpp
+++ src/libobjectmodel/test/instantiation.cpp
@@ -4,18 +4,52 @@
 #include <libobjectmodel/object_vector.hpp>
 #include <libobjectmodel/point_source.hpp>
 
+#include <boost/test/unit_test.hpp>
+
+#include <stdexcept>
 #include <iostream>
-#include <cstdlib>
 
-int main( int argc, char** argv )
+namespace visr
 {
-  using namespace visr::objectmodel;
+namespace objectmodel
+{
+namespace test
+{
 
-  PointSource foo( 1 );
+BOOST_AUTO_TEST_CASE( InstantiatePointSources )
+{
+  std::cout << "InstantiatePointSources started...." << std::endl;
+
 
   ObjectVector scene;
 
-  scene.set( 1, foo );
+  ObjectId const id1 = 1;
+  PointSource s1( id1 );
+  BOOST_CHECK_NO_THROW( scene.set( id1, s1 ) );
 
-  return EXIT_SUCCESS;
+  BOOST_CHECK( scene.size() == 1 );
+
+  ObjectId const id2 = 2;
+  PointSource s2( id2 );
+  BOOST_CHECK_NO_THROW( scene.set( id2, s2 ) );
+  BOOST_CHECK( scene.size() == 2 );
+
+  BOOST_CHECK_NO_THROW( scene.at( id1 ) );
+  AudioObject const & retrievedObj1 = scene.at( id1 );
+
+  BOOST_CHECK( s1.id() == retrievedObj1.id() );
+
+  // attempt access to source with non-existing ID
+  BOOST_CHECK_THROW( AudioObject const & retrievedObjInvld = scene.at( 5 ), std::exception );
+
+  PointSource s3( id1 );
+
+  BOOST_CHECK_NO_THROW( scene.set( id1, s3 ) ); // Replacing object at existing ID is legal
+  BOOST_CHECK( scene.size() == 2 ); // s3 replaces s1 at id1, size remains unchanged
+
+
 }
+
+} // namespace test
+} // namespace objectmodel
+} // namespce visr
---
+++ src/libobjectmodel/test/test_main.cpp
@@ -0,0 +1,7 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#define BOOST_TEST_MODULE "ObjectModel"
+
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
