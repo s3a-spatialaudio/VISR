c2c660441907e167a5cceffed12c2a5c6fa03c27
Andreas <a.franck@soton.ac.uk>
2015-01-28 18:12:14 +0000

    Added unit test for librcl (no functionality yet)

---
+++ src/librcl/test/scene_decoder.cpp
@@ -0,0 +1,30 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include <librcl/scene_decoder.hpp>
+
+#include <libril/audio_signal_flow.hpp>
+#include <libobjectmodel/object_vector.hpp>
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <stdexcept>
+#include <string>
+
+namespace visr
+{
+namespace rcl
+{
+namespace test
+{
+
+using namespace objectmodel;
+
+BOOST_AUTO_TEST_CASE( SceneDecoderInstantiation )
+{
+  // TODO: Implement me!
+}
+
+} // namespace test
+} // namespace rcl
+} // namespce visr
