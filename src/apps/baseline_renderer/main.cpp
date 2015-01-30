93f8824eeec7ef0fbdb599bb0deb1179bbae499d
Andreas <a.franck@soton.ac.uk>
2015-01-30 14:19:31 +0000

    Added application baseline_renderer
    * This is an enhanced version of the scene_decoder test application, with added command line options to control all configuration parameters.
    * TODO: Support re-assignment of channels indices for differeing sound card channnel layouts.

---
+++ src/apps/baseline_renderer/main.cpp
@@ -0,0 +1,134 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "signal_flow.hpp"
+#include "options.hpp"
+
+#include <libefl/denormalised_number_handling.hpp>
+
+#include <libpanning/LoudspeakerArray.h>
+
+#include <librrl/portaudio_interface.hpp>
+
+#include <boost/filesystem.hpp>
+
+#include <cstddef>
+#include <cstdlib>
+#include <cstdio> // for getc(), for testing purposes
+#include <iostream>
+#include <sstream>
+
+int main( int argc, char const * const * argv )
+{
+  using namespace visr;
+  using namespace visr::apps::baseline_renderer;
+
+  efl::DenormalisedNumbers::State const oldDenormNumbersState
+    = efl::DenormalisedNumbers::setDenormHandling();
+
+  Options cmdLineOptions;
+  std::stringstream errMsg;
+  switch( cmdLineOptions.parse( argc, argv, errMsg ) )
+  {
+  case Options::ParseResult::Failure:
+    std::cerr << "Error while parsing command line options: " << errMsg.str() << std::endl;
+    return EXIT_FAILURE;
+  case Options::ParseResult::Help:
+    cmdLineOptions.printDescription( std::cout );
+    return EXIT_SUCCESS;
+  case Options::ParseResult::Version:
+    // TODO: Implement retrieval of version information.
+    std::cout << "VISR S3A Baseline Renderer V0.1b" << std::endl;
+    return EXIT_SUCCESS;
+  case Options::ParseResult::Success:
+    break; // carry on
+  }
+
+  boost::filesystem::path const arrayConfigFile( cmdLineOptions.getOption<std::string>( "array-config" ) );
+  if( !exists( arrayConfigFile ) )
+  {
+    std::cerr << "The specified loudspeaker array configuration file \""
+      << arrayConfigFile.string( ).c_str( ) << "\" does not exist." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Create an Loudspeak
+  FILE * arrayConfigHandle = fopen( arrayConfigFile.string().c_str(), "r" );
+  if( not arrayConfigHandle )
+  {
+    std::cerr << "The specified loudspeaker array configuration file \""
+      << arrayConfigFile.string( ).c_str( ) << "\" could not be opened." << std::endl;
+  }
+  LoudspeakerArray loudspeakerArray;
+  if( loudspeakerArray.load( arrayConfigHandle ) != 0 )
+  {
+    std::cerr << "TError while parsing the specified loudspeaker array configuration file \""
+      << arrayConfigFile.string( ).c_str( ) << "\"." << std::endl;
+  }
+  const std::size_t numSpeakersFromArrayConfig = static_cast<std::size_t>(loudspeakerArray.getNumSpeakers());
+
+  const std::size_t numberOfOutputChannels
+    = cmdLineOptions.getDefaultedOption<std::size_t>( "output-channels", numSpeakersFromArrayConfig );
+
+  const std::size_t numberOfObjects = cmdLineOptions.getOption<std::size_t>( "input-channels" );
+  const std::size_t periodSize = cmdLineOptions.getDefaultedOption<std::size_t>( "period", 1024 );
+  const std::size_t samplingRate = cmdLineOptions.getDefaultedOption<std::size_t>( "sampling-frequency", 48000 );
+
+  const std::string audioBackend = cmdLineOptions.getDefaultedOption<std::string>( "audio-backend", "default" );
+
+  const std::size_t  sceneReceiverPort = cmdLineOptions.getDefaultedOption<std::size_t>( "scene-port", 8888 );
+
+  try 
+  {
+    rrl::PortaudioInterface::Config interfaceConfig;
+    interfaceConfig.mNumberOfCaptureChannels = numberOfObjects;
+    interfaceConfig.mNumberOfPlaybackChannels = numberOfOutputChannels;
+    interfaceConfig.mPeriodSize = periodSize;
+    interfaceConfig.mSampleRate = samplingRate;
+    interfaceConfig.mInterleaved = false;
+    interfaceConfig.mSampleFormat = rrl::PortaudioInterface::Config::SampleFormat::float32Bit;
+    interfaceConfig.mHostApi = audioBackend;
+
+    boost::filesystem::path const decoderDir( CMAKE_CURRENT_SOURCE_DIR );
+    boost::filesystem::path const configFile( "octahedron.txt" );
+    boost::filesystem::path const fullPath = decoderDir / configFile;
+    if( !exists( fullPath ) )
+    {
+      std::cerr << "The compiled-in configuration file \"" 
+                << fullPath.string().c_str() << "\" does not exist." << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    rrl::PortaudioInterface audioInterface( interfaceConfig );
+
+    const std::size_t cInterpolationLength = periodSize;
+
+    SignalFlow flow( numberOfObjects, numberOfOutputChannels,
+                     cInterpolationLength,
+                     fullPath.string().c_str(), sceneReceiverPort,
+                     periodSize, samplingRate );
+    flow.setup();
+
+    audioInterface.registerCallback( &ril::AudioSignalFlow::processFunction, &flow );
+
+    // should there be a separate start() method for the audio interface?
+    audioInterface.start( );
+
+    // Rendering runs until <Return> is entered on the console.
+    std::getc( stdin );
+
+    audioInterface.stop( );
+
+   // Should there be an explicit stop() method for the sound interface?
+
+    audioInterface.unregisterCallback( &ril::AudioSignalFlow::processFunction );
+  }
+  catch( std::exception const & ex )
+  {
+    std::cout << "Exception caught on top level: " << ex.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+ efl::DenormalisedNumbers::resetDenormHandling( oldDenormNumbersState );
+
+  return EXIT_SUCCESS;
+}
