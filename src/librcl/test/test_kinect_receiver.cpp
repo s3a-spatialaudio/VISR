ab37c1233fe2726f7c7cbcf4b969145a031f9a53
Andreas Franck <a.franck@soton.ac.uk>
2015-02-12 17:32:27 +0000

    Made rcl_test compilable after inclusion of test_kinect_receiver

--- src/librcl/test/test_kinect_receiver.cpp
+++ src/librcl/test/test_kinect_receiver.cpp
@@ -1,20 +1,17 @@
 /* Copyright Institute of Sound and Vibration Research - All rights reserved */
 
-#include <librcl/udp_receiver.hpp>
 #include <libpanning/LoudspeakerArray.h>
-
-#include <iostream>
-#include <cstdio>
-#include <stdlib.h>
-
+#include <librcl/udp_receiver.hpp>
 #include <libril/audio_signal_flow.hpp>
-#include <libobjectmodel/object_vector.hpp>
+#include <libpml/message_queue.hpp>
 
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem/path.hpp>
 
-
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include <stdexcept>
 #include <string>
 
@@ -22,41 +19,57 @@ namespace visr
 {
 namespace rcl
 {
-		namespace test
-		{
+namespace test
+{
 
-			using namespace objectmodel;
+class Flow: public ril::AudioSignalFlow
+{
+public:
+  explicit Flow( std::size_t period, ril::SamplingFrequencyType samplingFrequency )
+    : ril::AudioSignalFlow( period, samplingFrequency )
+  {
+  }
 
-			BOOST_AUTO_TEST_CASE(testListenerCompensation)
-			{
-				// TODO: Implement met
-				ril::AudioSignalFlow flow;
-			
-				rcl::UdpReceiver kinect( flow, "Receiver");
-				pml::MessageQueue<std::string> posMessage; //output position for the Kinect
-				const std::size_t kinectNetworkPort=8888;
+  /*virtual*/ void setup()
+  {
+  }
+  /*virtual*/ void process()
+  {
+  }                            
+};
+
+BOOST_AUTO_TEST_CASE(testKinectReceiver)
+{
+  // TODO: Implement me                          
+                          
+
+  Flow flow( 256, 48000 );
+  
+  rcl::UdpReceiver kinect( flow, "Receiver");
+  pml::MessageQueue<std::string> posMessage; //output position for the Kinect
+  const std::size_t kinectNetworkPort=8888;
 
 
-				kinect.setup(kinectNetworkPort, rcl::UdpReceiver::Mode::Synchronous); //setup to listen from th, how to set up mode?
+  kinect.setup(kinectNetworkPort, rcl::UdpReceiver::Mode::Synchronous); //setup to listen from th, how to set up mode?
 
-				while( true ) 
-				{ 
+  while( true ) 
+  { 
 
-					kinect.process(posMessage);
+    kinect.process(posMessage);
 
-					while (!posMessage.empty())
-					{
-						std::cout << posMessage.nextElement() << std::endl;
-						posMessage.popNextElement();
-					}
+    while (!posMessage.empty())
+    {
+      std::cout << posMessage.nextElement() << std::endl;
+      posMessage.popNextElement();
+    }
 
-					std::getc( std::cin );
-				}
+    std::getc( stdin );
+  }
 
 				
 
-			}
+}
 
-			} // namespace test
-	} // namespace rcl
+} // namespace test
+} // namespace rcl
 } // namespce visr
