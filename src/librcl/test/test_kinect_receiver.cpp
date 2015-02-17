72e6df229e620a21ada1f4ded347c8a456101dd8
Marcos Simon <mfsg1u13@soton.ac.uk>
2015-02-17 11:34:52 +0000

    Modified listener compensation which encapsulate the previous functions created by Marcos are added to the repository. although test_kinect_receiver si added; a test function for the creation of gains and delays with data received from the kinect

--- src/librcl/test/test_kinect_receiver.cpp
+++ src/librcl/test/test_kinect_receiver.cpp
@@ -1,9 +1,13 @@
 /* Copyright Institute of Sound and Vibration Research - All rights reserved */
 
 #include <libpanning/LoudspeakerArray.h>
+#include <libefl/basic_vector.hpp>
+#include <librcl/listener_compensation.hpp>
 #include <librcl/udp_receiver.hpp>
 #include <libril/audio_signal_flow.hpp>
 #include <libpml/message_queue.hpp>
+#include <libpml/listener_position.hpp>
+
 
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
@@ -13,6 +17,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <stdexcept>
+#include <sstream>
 #include <string>
 
 namespace visr
@@ -46,28 +51,50 @@ BOOST_AUTO_TEST_CASE(testKinectReceiver)
   Flow flow( 256, 48000 );
   
   rcl::UdpReceiver kinect( flow, "Receiver");
+  rcl::ListenerCompensation listenerComp( flow, "ListenerCompensation");
+
   pml::MessageQueue<std::string> posMessage; //output position for the Kinect
   const std::size_t kinectNetworkPort=8888;
 
+  std::size_t const numSpeakers = 5;
+
+  boost::filesystem::path const basePath(CMAKE_SOURCE_DIR);
+  boost::filesystem::path const arrayPath = basePath / boost::filesystem::path("src/libpanning/test/arrays/5.1_audiolab.txt");
+//  BOOST_CHECK_MESSAGE( boost::filesystem::exist(arrayPath), "The loudspeaker array file does not exist.");
+
+  pml::ListenerPosition pos;
+  efl::BasicVector<rcl::ListenerCompensation::SampleType> gains(numSpeakers, ril::cVectorAlignmentSamples );
+  efl::BasicVector<rcl::ListenerCompensation::SampleType> delays(numSpeakers, ril::cVectorAlignmentSamples );
+
 
   kinect.setup(kinectNetworkPort, rcl::UdpReceiver::Mode::Synchronous); //setup to listen from th, how to set up mode?
+  listenerComp.setup(numSpeakers, arrayPath.string());
 
   while( true ) 
   { 
 
     kinect.process(posMessage);
 
-    while (!posMessage.empty())
+	// We parse all arriving messages and keep only the most recent.
+	while (!posMessage.empty())
     {
-      std::cout << posMessage.nextElement() << std::endl;
-      posMessage.popNextElement();
+	  // put out the strings:
+      // std::cout << posMessage.nextElement() << std::endl;
+
+	  std::stringstream messageStream(posMessage.nextElement());
+	  pos.parse(messageStream);
+	  //std::cout << "Received position: " << pos << std::endl;
+
+      posMessage.popNextElement();//remove oldest message from queue
     }
+	// We parsed all arriving messages and kept only the most recent.
+	
+	listenerComp.process(pos, gains, delays);
+	std::cout << "Gains: " << gains[0] << "Delays: " << delays[0] << std::endl;
 
-    std::getc( stdin );
+
+//    std::getc( stdin );
   }
-
-				
-
 }
 
 } // namespace test
