1919f165298a63611331925461bb19f5d2abc904
Andreas <a.franck@soton.ac.uk>
2015-02-18 11:44:02 +0000

    Work on extensions of the object model
    * Introduction of new source types
    * Test and cleanup pending.

---
+++ src/libobjectmodel/point_source_with_diffuseness_parser.cpp
@@ -0,0 +1,37 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "point_source_with_diffuseness_parser.hpp"
+
+#include "point_source_parser.hpp"
+#include "point_source_with_diffuseness.hpp"
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace visr
+{
+namespace objectmodel
+{
+
+/*virtual*/ void PointSourceWithDiffusenessParser::
+parse( boost::property_tree::ptree const & tree, Object & src ) const
+{
+  try
+  {
+    // note: cannot check for object type id since src might be a subclass of PointSource
+    PointSourceWithDiffuseness & diffusePointSrc = dynamic_cast<PointSourceWithDiffuseness&>(src);
+
+    // Parse all members inherited from the base class PointSource
+    this->PointSourceParser::parse( tree, diffusePointSrc );
+
+    // parse point source-specific data members
+    diffusePointSrc.setDiffuseness( tree.get<PointSource::Coordinate>( "diffuseness" ) );
+  }
+  // TODO: distinguish between boost property_tree parse errors and bad dynamic casts.
+  catch( std::exception const & ex )
+  {
+    throw std::invalid_argument( std::string( "Error while parsing point source: object: ") + ex.what() );
+  }
+}
+
+} // namespace objectmodel
+} // namespace visr
