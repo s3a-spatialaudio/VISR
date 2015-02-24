a9c6b777c4a92456fe0fa9aa63cd8826c855531a
Marcos Simon <mfsg1u13@soton.ac.uk>
2015-02-24 14:08:48 +0000

    Mended listener_compensation.cpp and added stereo_audiolab.txt loudspeaker set up

--- src/librcl/test/test_kinect_receiver.cpp
+++ src/librcl/test/test_kinect_receiver.cpp
@@ -58,10 +58,10 @@ BOOST_AUTO_TEST_CASE(testKinectReceiver)
   pml::MessageQueue<std::string> posMessage; //output position for the Kinect
   const std::size_t kinectNetworkPort=8888;
 
-  std::size_t const numSpeakers = 5;
+  std::size_t const numSpeakers = 2;
 
   boost::filesystem::path const basePath(CMAKE_SOURCE_DIR);
-  boost::filesystem::path const arrayPath = basePath / boost::filesystem::path("src/libpanning/test/arrays/5.1_audiolab.txt");
+  boost::filesystem::path const arrayPath = basePath / boost::filesystem::path("src/libpanning/test/arrays/stereo_audiolab.txt");
 //  BOOST_CHECK_MESSAGE( boost::filesystem::exist(arrayPath), "The loudspeaker array file does not exist.");
 
   pml::ListenerPosition pos;
@@ -80,6 +80,33 @@ BOOST_AUTO_TEST_CASE(testKinectReceiver)
     std::cout << "Decoded position message: " << pos << std::endl;
     listenerComp.process( pos, gains, delays );
     std::cout << "Gains: " << gains[0] << "Delays: " << delays[0] << std::endl;
+	std::cout << "Gains: " << gains[1] << "Delays: " << delays[1] << std::endl;
+	//std::cout << "Gains: " << gains[2] << "Delays: " << delays[2] << std::endl;
+	//std::cout << "Gains: " << gains[3] << "Delays: " << delays[3] << std::endl;
+	//std::cout << "Gains: " << gains[4] << "Delays: " << delays[4] << std::endl;
+	//std::cout << "Gains: " << gains[5] << "Delays: " << delays[5] << std::endl;
+	//std::cout << "Gains: " << gains[6] << "Delays: " << delays[6] << std::endl;
+	//std::cout << "Gains: " << gains[7] << "Delays: " << delays[7] << std::endl;
+	//std::cout << "Gains: " << gains[8] << "Delays: " << delays[8] << std::endl;
+	//std::cout << "Gains: " << gains[9] << "Delays: " << delays[9] << std::endl;
+	//std::cout << "Gains: " << gains[10] << "Delays: " << delays[10] << std::endl;
+	//std::cout << "Gains: " << gains[11] << "Delays: " << delays[11] << std::endl;
+	//std::cout << "Gains: " << gains[12] << "Delays: " << delays[12] << std::endl;
+	//std::cout << "Gains: " << gains[13] << "Delays: " << delays[13] << std::endl;
+	//std::cout << "Gains: " << gains[14] << "Delays: " << delays[14] << std::endl;
+	//std::cout << "Gains: " << gains[15] << "Delays: " << delays[15] << std::endl;
+	//std::cout << "Gains: " << gains[16] << "Delays: " << delays[16] << std::endl;
+	//std::cout << "Gains: " << gains[17] << "Delays: " << delays[17] << std::endl;
+
+
+
+
+
+
+
+
+
+
   }
 }
 
