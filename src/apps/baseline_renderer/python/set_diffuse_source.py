8210c6fc75c0ca5a5c4fb685a3fa1cd4aad1b5c4
Andreas Franck <a.franck@soton.ac.uk>
2015-03-09 17:44:17 +0000

    Adapted Python scripts to new source types.
    * New script for purely diffuse sources
    * Added diffuseness parameter to point source script (differentiates between the source types depending on the set diffuseness)

--- src/apps/baseline_renderer/python/set_diffuse_source.py
+++ src/apps/baseline_renderer/python/set_diffuse_source.py
@@ -8,7 +8,6 @@ parser = argparse.ArgumentParser()
 
 # The positional (mandatory) arguments:
 parser.add_argument( "objectid", help="The object id, an integer >= 0." )
-parser.add_argument( "position", help="The object position in Cartesian coordinates, specified as x:y:z or x:y." )
 
 parser.add_argument( "-v", "--verbosity", action="count", default=0, help="Print the network message after sending." )
 parser.add_argument( "-r", "--receiver", default="localhost:4242", help="The destination network address, format: host:port, default: localhost:4242." )
@@ -16,7 +15,6 @@ parser.add_argument( "-l", "--level", default=1.0, help="Sound level of the sour
 parser.add_argument( "-p", "--priority", default="0", help="Rendering priority, integer >= 0, default: 0." );
 parser.add_argument( "-c", "--channelid", default="-1", help="Channel id, denotes the audio channel for the source signal. Integer >=0. Default: -1 meaning that the object id is used as channel id.");
 parser.add_argument( "-g", "--groupid", default="0", help="Group id, integer >= 0." );
-parser.add_argument( "-d", "--diffuseness", default="0.0", help="Diffuseness, floating point value 0 <= d <= 1" );
 
 args = parser.parse_args()
 
@@ -25,14 +23,6 @@ destHost = destStrs[0]
 destPort = int( destStrs[1] )
 
 objectId = int(args.objectid)
-
-pos = args.position.split(":")
-posX = float(pos[0])
-posY = float(pos[1])
-if len( pos ) == 3:
-  posZ = float(pos[2])
-else:
-  posZ = 0.0
 
 if int(args.channelid) < 0:
   channelId = objectId
@@ -45,15 +35,8 @@ priority = int(args.priority)
 
 level = float( args.level )
 
-diffuseness = float( args.diffuseness )
 
-print( "Diffuseness: %f" % diffuseness )
-
-if (diffuseness == 0.0):
-  msg = "{ \"objects\":[{\"id\": %d, \"channels\": %d,\n\"type\": \"point\", \"group\": %d, \"priority\": %d, \"level\": %f, \"position\": {\"x\": %f, \"y\": %f, \"z\": %f } }]  }" % ( objectId, channelId, groupId, priority, level, posX, posY, posZ )
-else:
-  msg = "{ \"objects\":[{\"id\": %d, \"channels\": %d,\n\"type\": \"pointdiffuse\", \"group\": %d, \"priority\": %d, \"level\": %f, \"diffuseness\": %f, \"position\": {\"x\": %f, \"y\": %f, \"z\": %f } }]  }" % ( objectId, channelId, groupId, priority, level, diffuseness, posX, posY, posZ )
-
+msg = "{ \"objects\":[{\"id\": %d, \"channels\": %d,\n\"type\": \"diffuse\", \"group\": %d, \"priority\": %d, \"level\": %f}]  }" % ( objectId, channelId, groupId, priority, level )
 
 udpSocket = socket.socket(socket.AF_INET, # Internet
                           socket.SOCK_DGRAM) # UDP
