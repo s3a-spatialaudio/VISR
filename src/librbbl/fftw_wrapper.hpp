a2822de0a0ed694b777d62ebd18b08a6124c890a
Andreas <a.franck@soton.ac.uk>
2015-04-27 00:22:41 +0100

    Changed names {forward|inverse}Fft to {forward|inverse}Transform, use std::complex for frequency-domain representation.

--- src/librbbl/fftw_wrapper.hpp
+++ src/librbbl/fftw_wrapper.hpp
@@ -1,3 +1,4 @@
+
 /* Copyright Institute of Sound and Vibration Research - All rights reserved */
 
 #ifndef VISR_LIBRBBL_FFTW_WRAPPER_HPP_INCLUDED
@@ -26,9 +27,9 @@ public:
 
   /*virtual*/ void init( std::size_t fftSize, std::size_t alignment );
 
-  /*virtual*/ void forwardFft( DataType const * const in, DataType * out );
+  /*virtual*/ void forwardTransform( DataType const * const in, FrequencyDomainType * out );
 
-  /*virtual*/ void inverseFft( DataType const * const in, DataType * out );
+  /*virtual*/ void inverseTransform( FrequencyDomainType const * const in, DataType * out );
 private:
   /**
    * Internal implementation object to avoid FFTW dependencies in the header.
