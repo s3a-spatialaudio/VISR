dda9a58d1c64a3eb994ab12c2de1639ecb60f3aa
Andreas Franck <a.franck@soton.ac.uk>
2015-05-06 15:20:02 +0100

    Added first version of KissFFT
    * Platform-specific storage of libraries not implemented yet.
    * Heavy modifications were made to the KissFFT library in order to make both the float and double version available.
    * The modified KissFT sources are stored in ssh://ssh.soton.ac.uk/research/s3adata/repos/kissfft

---
+++ 3rd/kissfft/include/kiss_fftr_double_redefines.h
@@ -0,0 +1,9 @@
+// Copyright Andreas Franck - All rights reserved
+
+#define kiss_fft_scalar double
+
+#define kiss_fftr_state kiss_fftr_state_double
+#define kiss_fftr_cfg kiss_fftr_cfg_double
+#define kiss_fftr_alloc kiss_fftr_alloc_double
+#define kiss_fftr kiss_fftr_double
+#define kiss_fftri kiss_fftri_double
