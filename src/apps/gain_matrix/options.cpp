ab3212bbffd0ef898ac6fe275eb221db518527f4
Andreas <a.franck@soton.ac.uk>
2015-12-14 11:41:50 +0000

    Refactored gain matrix application to perform a real-world task.
    * Now general tool to do matrixing
    * Added option handling
    * Code cleanup

---
+++ src/apps/gain_matrix/options.cpp
@@ -0,0 +1,71 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "options.hpp"
+
+#include <cstdint>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+
+namespace visr
+{
+namespace apps
+{
+namespace gain_matrix
+{
+
+Options::Options()
+{
+  registerOption<bool>( "help,h", "Show help and usage information." );
+  registerOption<bool>( "version,v", "Display version information." );
+
+  registerOption<std::string>( "audio-backend,D", "The audio backend." );
+  registerOption<std::size_t>( "sampling-frequency,r", "Sampling frequency [Hz]" );
+  registerOption<std::size_t>( "period,p", "Period (blocklength) [Number of samples per audio block]" );
+
+  registerOption<std::string>( "matrix,m", "Initial gain matrix" );
+  registerOption<std::string>( "matrix-file,f", "Initial gain matrix as a text file." );
+  registerOption<double>( "global-gain,g", "Overall gain modification (in dB) applied to the initial matrix." );
+}
+
+Options::~Options()
+{
+}
+
+Options::ParseResult Options::parse( int argc, char const * const * argv, std::ostream & errMsg )
+{
+  try
+  {
+    boost::program_options::store( boost::program_options::parse_command_line( argc, argv, mDescription ),
+                                   mVariablesMap );
+    notify( mVariablesMap );
+  }
+  catch( std::exception const & ex )
+  {
+    errMsg << ex.what();
+    return ParseResult::Failure;
+  }
+  if( getOption<bool>( "help" ) )
+  {
+    return ParseResult::Help;
+  }
+  if( getOption<bool>( "version" ) )
+  {
+    return ParseResult::Version;;
+  }
+  return ParseResult::Success;
+}
+  
+bool Options::hasOption( char const * optionName ) const
+{
+  return (mVariablesMap.count( optionName ) >= 1);
+}
+
+void Options::printDescription( std::ostream & out )
+{
+  out << mDescription << std::endl;
+}
+
+} // namespace gain_matrix
+} // namespace apps
+} // namespace visr
--- src/apps/gain_matrix/signal_flow.cpp
+++ src/apps/gain_matrix/signal_flow.cpp
@@ -12,7 +12,9 @@ namespace apps
 namespace gain_matrix
 {
 
-// create a helper function in an unnamed namespace
+namespace // unnamed
+{
+  // create a helper function in an unnamed namespace
   std::vector<std::size_t> indexRange( std::size_t startIdx, std::size_t endIdx )
   {
     if( endIdx < startIdx )
@@ -24,9 +26,11 @@ namespace gain_matrix
     std::generate( ret.begin(), ret.end(), [&] { return startIdx++; } );
     return ret;
   }
+}
 
 SignalFlow::SignalFlow( std::size_t numberOfInputs,
   std::size_t numberOfOutputs,
+  pml::MatrixParameter<ril::SampleType> const & initialMatrix,
   std::size_t interpolationPeriod,
   std::size_t period, ril::SamplingFrequencyType samplingFrequency )
  : AudioSignalFlow( period, samplingFrequency )
@@ -34,8 +38,28 @@ SignalFlow::SignalFlow( std::size_t numberOfInputs,
  , cNumberOfOutputs( numberOfOutputs )
  , cInterpolationSteps( interpolationPeriod )
  , mMatrix( *this, "GainMatrix" )
- , mCounter( 0 )
 {
+  mMatrix.setup( cNumberOfInputs, cNumberOfOutputs, cInterpolationSteps, initialMatrix );
+
+  initCommArea( cNumberOfInputs + cNumberOfOutputs, period, ril::cVectorAlignmentSamples );
+
+  // connect the ports
+  assignCommunicationIndices( "GainMatrix", "in", indexRange( 0, cNumberOfInputs - 1 ) );
+
+  assignCommunicationIndices( "GainMatrix", "out", indexRange( cNumberOfInputs, cNumberOfInputs + cNumberOfOutputs - 1 ) );
+
+  // Set the indices for communicating the signals from and to the outside world.
+  std::vector<ril::AudioPort::SignalIndexType> captureIndices = indexRange( 0, cNumberOfInputs - 1 );
+  std::vector<ril::AudioPort::SignalIndexType> playbackIndices = indexRange( cNumberOfInputs, cNumberOfInputs + cNumberOfOutputs - 1 );
+
+  assignCaptureIndices( &captureIndices[0], captureIndices.size( ) );
+  assignPlaybackIndices( &playbackIndices[0], playbackIndices.size( ) );
+
+  assignCaptureIndices( indexRange( 0, cNumberOfInputs - 1 ) );
+  assignPlaybackIndices( indexRange( cNumberOfInputs, cNumberOfInputs + cNumberOfOutputs - 1 ) );
+
+  // should not be done here, but in AudioSignalFlow where this method is called.
+  setInitialised( true );
 }
 
 SignalFlow::~SignalFlow( )
@@ -45,47 +69,7 @@ SignalFlow::~SignalFlow( )
 /*virtual*/ void 
 SignalFlow::process()
 {
-  if( ++mCounter % 128 == 0 )
-  {
-    mMatrix.setGains( mCounter % 256 == 0 ? mNewMtx1 : mNewMtx2 );    
-  }
   mMatrix.process();
-}
-
-/*virtual*/ void 
-SignalFlow::setup()
-{
-  // Initialise and configure audio components
-  mMatrix.setup( cNumberOfInputs, cNumberOfOutputs, cInterpolationSteps, 1.0f );
-
-  initCommArea( cNumberOfInputs + cNumberOfOutputs, period( ), ril::cVectorAlignmentSamples );
-
-  // connect the ports
-  assignCommunicationIndices( "GainMatrix", "in", indexRange( 0, cNumberOfInputs-1 ) );
-
-  assignCommunicationIndices( "GainMatrix", "out", indexRange( cNumberOfInputs, cNumberOfInputs + cNumberOfOutputs - 1 ) );
-
-  // Set the indices for communicating the signals from and to the outside world.
-  std::vector<ril::AudioPort::SignalIndexType> captureIndices = indexRange( 0, cNumberOfInputs - 1 );
-  std::vector<ril::AudioPort::SignalIndexType> playbackIndices = indexRange( cNumberOfInputs, cNumberOfInputs + cNumberOfOutputs - 1 );
-
-  assignCaptureIndices( &captureIndices[0], captureIndices.size() );
-  assignPlaybackIndices( &playbackIndices[0], playbackIndices.size( ) );
-
-  assignCaptureIndices( indexRange( 0, cNumberOfInputs - 1 ) );
-  assignPlaybackIndices( indexRange( cNumberOfInputs, cNumberOfInputs + cNumberOfOutputs - 1 ) );
-
-  mNewMtx1.resize( cNumberOfOutputs, cNumberOfInputs );
-  mNewMtx1( 0, 0 ) = 0.5f;
-  mNewMtx1( 1, 1 ) = 0.5f;
-
-  mNewMtx2.resize( cNumberOfOutputs, cNumberOfInputs );
-  mNewMtx2( 0, 1 ) = 0.1f;
-  mNewMtx2( 1, 0 ) = 0.1f;
-
-
-  // should not be done here, but in AudioSignalFlow where this method is called.
-  setInitialised( true );
 }
 
 } // namespace gain_matrix
