5f87e3adaaf24d3e1c9affaa73b7f00958e3f101
Andreas <a.franck@soton.ac.uk>
2016-04-24 19:48:15 +0100

    - Removed shorthand option -f for 'filters'
    * Also cleanup of description strings.

--- src/apps/convolver_matrix/options.cpp
+++ src/apps/convolver_matrix/options.cpp
@@ -26,22 +26,22 @@ Options::Options()
   registerOption<bool>( "list-fft-libraries", "List the supported FFT implementations that can be selected using the \"--fftLibrary\" option." );
 
   registerOption<std::size_t>( "sampling-frequency,f", "Sampling frequency [Hz]" );
-  registerOption<std::size_t>( "period,p", "Period (blocklength): The number of samples per audio block, also the block size of the partitioned convolution." );
+  registerOption<std::size_t>( "period,p", "Period (block length): The number of samples per audio block, also the block size of the partitioned convolution." );
 
-  registerOption<std::size_t>( "input-channels,i", "Number of input channels for audio object signal" );
-  registerOption<std::size_t>( "output-channels,o", "Number of audio output channels" );
+  registerOption<std::size_t>( "input-channels,i", "Number of input channels for audio object signal." );
+  registerOption<std::size_t>( "output-channels,o", "Number of audio output channels." );
 
-  registerOption<std::string>( "filters,f", "Initial impulse responses, specified as comma-separated list of one or multiple WAV files." );
+  registerOption<std::string>( "filters", "Initial impulse responses, specified as comma-separated list of one or multiple WAV files." );
 
-  registerOption<std::string>( "filter-file-index-offsets", "Index offsets to address the impulses in the provideed multichannel filter files."
+  registerOption<std::string>( "filter-file-index-offsets", "Index offsets to address the impulses in the provided multichannel filter files."
     " If specified, the number of values must match the number of filter files." );
 
-  registerOption<std::string>( "routings,r", "Initial routing entries, provided" );
+  registerOption<std::string>( "routings,r", "Initial routing entries, expects a JSON array consisting of objects \"{\"inputs\": nn, \"outputs\":nn, \"filters\":nn (\"gain\":XX)" );
   registerOption<std::size_t>( "max-filter-length,l", "Maximum length of the impulse responses, in samples."
     " If not given, it defaults to the longest provided filter, " );
 
-  registerOption<std::size_t>( "max-routings", "Maximum number of filter routings" );
-  registerOption<std::size_t>( "max-filters", "Maximum number of impulse responses that can be stored" );
+  registerOption<std::size_t>( "max-routings", "Maximum number of filter routings." );
+  registerOption<std::size_t>( "max-filters", "Maximum number of impulse responses that can be stored." );
     
   registerOption<std::string>( "fft-library", "Specify the FFT implementation to be used. Defaults to the default implementation for the platform." );
 }
