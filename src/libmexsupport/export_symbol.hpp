835efd201d515b5b6ff3e4deecf470d8155ab20e
Andreas <a.franck@soton.ac.uk>
2016-04-26 12:25:06 +0100

    Changed implementation for Windows
    * We need to rely in the extra .def file there.
    * That also means that we will not be able to use the same generic export control facility as for other parts of the system.

--- src/libmexsupport/export_symbol.hpp
+++ src/libmexsupport/export_symbol.hpp
@@ -3,17 +3,18 @@
 #ifndef VISR_MEXSUPPORT_EXPORT_SYMBOL_HPP_INCLUDED
 #define VISR_MEXSUPPORT_EXPORT_SYMBOL_HPP_INCLUDED
 
-/**
- * Export a symbol or a class defined in a shared library/module.
- * This is used to control the exported interface of librariesand make it platform-independent
- * despite the different default visibilities of Windows and Unix-based platforms.
- * This assumes that the default visibility on Unix platforms is changed to hidden (as currently
- * done in the top-level makefile.
- * @todo Make this a general library utility (not restricted to mex)
- */
 #ifdef _MSC_VER
-#define VISR_MEXSUPPORT_EXPORT_SYMBOL __declspec(dllexport)
+#define VISR_MEXSUPPORT_EXPORT_SYMBOL
 #else
+/**
+* Export a symbol or a class defined in a shared library/module.
+* This is used to control the exported interface of librariesand make it platform-independent
+* despite the different default visibilities of Windows and Unix-based platforms.
+* This assumes that the default visibility on Unix platforms is changed to hidden (as currently
+* done in the top-level makefile.
+* @note On Windows, Matlab requires that the export properties are set via an additional .def file. Therefore 
+* we need to define this as empty.
+*/
 #define VISR_MEXSUPPORT_EXPORT_SYMBOL __attribute__((__visibility__("default")))
 #endif
 
