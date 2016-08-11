48070e3078247c2c82aea8b225411747b68722ec
Andreas <a.franck@soton.ac.uk>
2016-08-11 10:11:33 +0100

    Made gain_matrix~ Max/MSP external working
    * Initialisation of gain matrices from strings and text (Matlab) files.
    * Runtime gain changes not supported at the moment.

--- src/maxmspexternals/gain_matrix/gain_matrix.hpp
+++ src/maxmspexternals/gain_matrix/gain_matrix.hpp
@@ -1,30 +1,22 @@
 /* Copyright Institute of Sound and Vibration Research - All rights reserved */
 
-/* Super-safe determination of the MAX define for setting the operating system. */
-#ifdef __APPLE_CC__
-#ifndef MAC_VERSION 
-#define MAC_VERSION
-#undef WIN_VERSION
-#endif
-#else
-#ifdef _MSC_VER
-#ifndef WIN_VERSION 
-#define WIN_VERSION
-#endif
-#undef MAC_VERSION
-#endif
-#endif
-
 #include <maxmspexternals/libmaxsupport/external_base.hpp>
 #include <maxmspexternals/libmaxsupport/signal_flow_wrapper.hpp>
 
 #include <libsignalflows/gain_matrix.hpp>
 
+// Some loony (Max?) obviously defines max, thus hiding any C++/STL functions of the same name.
+#undef max
+#undef min // same for min(), although there is no Min/MSP around.
+
 #include <libpml/matrix_parameter.hpp>
 
 #include <cstddef>
-#include <limits>
 #include <memory>
+
+// Some loony (Max?) obviously defines max, thus hiding any C++/STL functions of the same name.
+#undef max
+#undef min // same for min(), although there is no Min/MSP around.
 
 namespace visr
 {
