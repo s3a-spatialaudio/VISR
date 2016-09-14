8080bbdfa426d60523a66a235d3a99f416e604ab
Andreas <a.franck@soton.ac.uk>
2016-09-14 17:38:32 +0100

    Added ParameterConnectionMap type plus corresponding functions.

---
+++ src/librrl/parameter_connection_map.cpp
@@ -0,0 +1,11 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "parameter_connection_map.hpp"
+
+namespace visr
+{
+namespace rrl
+{
+
+} // namespace rrl
+} // namespace visr
---
+++ src/librrl/parameter_connection_map.hpp
@@ -0,0 +1,35 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#ifndef VISR_LIBRRL_PARAMETER_CONNECTION_MAP_HPP_INCLUDED
+#define VISR_LIBRRL_PARAMETER_CONNECTION_MAP_HPP_INCLUDED
+
+#include <libril/constants.hpp>
+
+#include <iosfwd>
+#include <map>
+#include <memory>
+#include <set>
+#include <stdexcept>
+#include <vector>
+
+namespace visr
+{
+// Forward declarations
+namespace ril
+{
+class ParameterPortBase;
+}
+
+namespace rrl
+{
+
+using ParameterConnectionMap = std::multimap< ril::ParameterPortBase *, ril::ParameterPortBase * >;
+
+ParameterConnectionMap && resolvePlaceholders( ParameterConnectionMap const & fullConnections );
+
+std::ostream & operator<<(std::ostream & stream, ParameterConnectionMap const & connections);
+
+} // namespace rrl
+} // namespace visr
+
+#endif // #ifndef VISR_LIBRRL_PARAMETER_CONNECTION_MAP_HPP_INCLUDED
