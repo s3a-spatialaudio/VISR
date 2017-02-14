6a15bef7928694654d7b640b07d72338568505f9
Andreas <a.franck@soton.ac.uk>
2017-02-14 09:34:46 +0000

    Added API version functions.

---
+++ src/libril/version.cpp
@@ -0,0 +1,67 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "version.hpp"
+
+#include <algorithm>
+#include <iterator>
+#include <set>
+#include <sstream>
+
+namespace visr
+{
+namespace version
+{
+
+unsigned int major()
+{
+  return VISR_API_VERSION_MAJOR;
+}
+
+
+unsigned int minor()
+{
+  return VISR_API_VERSION_MINOR;
+}
+
+
+unsigned int patch()
+{
+  return VISR_API_VERSION_PATCH;
+}
+
+std::string versionString()
+{
+  std::stringstream stream;
+  stream << VISR_API_VERSION_MAJOR << '.' << VISR_API_VERSION_MINOR << '.' << VISR_API_VERSION_PATCH;
+  return stream.str();
+}
+
+
+namespace // unnamed
+{
+
+using FeatureSet = std::set< std::string>;
+
+// Avoid potential static initialization order fiascos by constructing the static feature list inside a function.
+FeatureSet const & featureSet()
+{
+  static const FeatureSet sFeatures{ "basic", "dynamic" }; // Just for testing
+  return sFeatures;
+}
+
+}
+
+std::string features()
+{
+  std::stringstream stream;
+  std::copy( featureSet().cbegin(), featureSet().cend(), std::ostream_iterator<std::string>(stream, "," ) );
+  return stream.str();
+}
+
+bool hasFeature( std::string const & feature )
+{
+  return featureSet().find( feature ) != featureSet().cend();
+}
+
+}
+}
