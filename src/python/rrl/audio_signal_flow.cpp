90a8955eef1c2f7f0f8bb3fce6cf33265e0321a1
Andreas <a.franck@soton.ac.uk>
2017-02-26 20:51:13 +0000

    First working verison of AudioSignalFlow::process() binding.

--- src/python/rrl/audio_signal_flow.cpp
+++ src/python/rrl/audio_signal_flow.cpp
@@ -6,12 +6,14 @@
 
 #ifdef USE_PYBIND11
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 #else
 #include <boost/python.hpp>
 #include <boost/python/args.hpp>
 #include <boost/noncopyable.hpp>
 #endif
 
+#include <vector>
 
 namespace visr
 {
@@ -25,6 +27,56 @@ namespace rrl
 
 #ifdef USE_PYBIND11
 
+
+pybind11::array_t<ril::SampleType> wrapProcess( visr::rrl::AudioSignalFlow & flow, pybind11::array const & input )
+{
+  using DataType = ril::SampleType; // Possibly replace by a template parameter later.
+
+  if( input.dtype() != pybind11::dtype::of<DataType>() )
+  {
+    throw std::invalid_argument( "AudioSignalFlow::process(): The data type input matrix does not match the used sample data type." );
+  }
+  if( input.ndim() != 2 )
+  {
+    throw std::invalid_argument( "AudioSignalFlow::process(): The input matrix is not 2D" );
+  }
+  if( input.shape(0) != flow.numberOfCaptureChannels() )
+  if( input.shape(1) != flow.period() )
+  {
+    throw std::invalid_argument( "AudioSignalFlow::process(): Dimension 1 input of the input matrix does not match the block size of the signal flow." );
+  }
+  if( input.strides(1) != sizeof(ril::SampleType) )
+  {
+    throw std::invalid_argument( "AudioSignalFlow::process(): Dimension 1 input of the input matrix must be continuous (row-major)." );
+  }
+  std::vector<ril::SampleType const *> inPtrs( flow.numberOfCaptureChannels(), nullptr );
+  std::size_t const inChannelStride = input.strides(0) / sizeof(DataType);
+  ril::SampleType const * inPtr = static_cast<ril::SampleType const *>( input.data() );
+  for( auto & el : inPtrs )
+  {
+    el = inPtr;
+    inPtr += inChannelStride;
+  }
+
+  // Allocate the output array.
+  pybind11::array outputSignal( pybind11::dtype::of<ril::SampleType>(),
+				{ flow.numberOfPlaybackChannels(), flow.period() },
+				{ sizeof(ril::SampleType)*flow.period(), sizeof(ril::SampleType) } // TODO: Take care of alignment
+                              );
+  std::vector<ril::SampleType *> outPtrs( flow.numberOfPlaybackChannels(), nullptr );
+  std::size_t const outChannelStride = outputSignal.strides(0) / sizeof(DataType);
+  ril::SampleType * outPtr = static_cast<ril::SampleType *>( outputSignal.mutable_data() );
+  for( auto & el : outPtrs )
+  {
+    el = outPtr;
+    outPtr += outChannelStride;
+  }
+
+  flow.process( &inPtrs[0], &outPtrs[0] );
+
+  return outputSignal;
+}
+
 void exportAudioSignalFlow( pybind11::module & m )
 {
   pybind11::class_<AudioSignalFlow>( m, "AudioSignalFlow" )
@@ -34,8 +86,9 @@ void exportAudioSignalFlow( pybind11::module & m )
    .def_property_readonly( "numberOfAudioPlaybackPorts", &AudioSignalFlow::numberOfAudioPlaybackPorts )
    .def_property_readonly( "numberOfCaptureChannels", &AudioSignalFlow::numberOfCaptureChannels )
    .def_property_readonly( "numberOfPlaybackChannels", &AudioSignalFlow::numberOfPlaybackChannels )
-    .def( "audioCapturePortName", &AudioSignalFlow::audioCapturePortName, pybind11::arg("index"), pybind11::return_value_policy::reference )
-    .def( "audioPlaybackPortName", &AudioSignalFlow::audioPlaybackPortName, pybind11::arg( "index" ), pybind11::return_value_policy::reference )
+   .def( "audioCapturePortName", &AudioSignalFlow::audioCapturePortName, pybind11::arg("index"), pybind11::return_value_policy::reference )
+   .def( "audioPlaybackPortName", &AudioSignalFlow::audioPlaybackPortName, pybind11::arg( "index" ), pybind11::return_value_policy::reference )
+   .def( "process", [](visr::rrl::AudioSignalFlow & flow, pybind11::array const & input) /*-> pybind11::array_t<ril::SampleType>*/ { return wrapProcess( flow, input );}, pybind11::return_value_policy::take_ownership )
   ;
 }
 
