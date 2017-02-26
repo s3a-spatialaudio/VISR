d1756f9e78748d0edf57020f2ea58b2d04a2ba6e
Andreas <a.franck@soton.ac.uk>
2017-02-26 19:19:11 +0000

    Added new script "runAtomicComponent"
    * Cleanup in instantiateBaselineRenderer

--- src/python/scripts/instantiateBaselineRenderer.py
+++ src/python/scripts/instantiateBaselineRenderer.py
@@ -6,7 +6,7 @@ Created on Tue Feb 14 15:59:11 2017
 """
 
 import sys
-sys.path.append( 'c://local/visr-build/python/Debug/' )
+sys.path.append( '/home/andi/dev/visr-build-debug/python' )
 
 import visr
 import signalflows
@@ -16,7 +16,7 @@ import rrl
 
 ctxt = visr.SignalFlowContext( 1024, 48000 )
 
-lc = pml.LoudspeakerArray( 'c:\\local\\visr\\config\\isvr\\audiolab_39speakers_1subwoofer.xml' )
+lc = pml.LoudspeakerArray( '/home/andi/dev/visr/config/isvr/audiolab_39speakers_1subwoofer.xml' )
 
 diffFilters = pml.MatrixParameterFloat( 39, 512, 16 )
 
@@ -26,30 +26,29 @@ print( 'Created renderer.' )
 
 flow1 = rrl.AudioSignalFlow( renderer1 )
 
-myFirstComposite = visr.CompositeComponent( ctxt, 'composite1', None )
+cc1 = visr.CompositeComponent( ctxt, 'composite1', None )
 
-numComponents = myFirstComposite.numberOfComponents
+numComponents = cc1.numberOfComponents
 
 print( 'Composite has %d components' % ( numComponents) )
 
-add1 = rcl.Add( ctxt, 'adder1', myFirstComposite )
+add1 = rcl.Add( ctxt, 'adder1', cc1 )
 add1.setup( width = 8, numInputs = 2 )
 
-add2 = rcl.Add( ctxt, 'adder2', myFirstComposite )
+add2 = rcl.Add( ctxt, 'adder2', cc1 )
 add2.setup( width = 8, numInputs = 2 )
 
 # In C++, this is a protected method (can be done only from within.)
-r1 = range(0,8)
-l1 = [0,1,2,3,4,5,6,7]
-myFirstComposite.registerAudioConnection( "adder1", "output", r1, "adder2", "in0", r1 )
+#r1 = range(0,8)
+#l1 = [0,1,2,3,4,5,6,7]
+cc1.registerAudioConnection( "adder1", "out", visr.ChannelList( visr.ChannelRange(0,8)), "adder2", "in0", visr.ChannelList( visr.ChannelRange(0,8)) )
 
-numComponents2 = myFirstComposite.numberOfComponents
+numComponents2 = cc1.numberOfComponents
 
 print( 'Composite has now %d components' % ( numComponents2) )
 
-
-# TODO: Add checking methods to composite components.
-
-# Do not attempt this unless the 
-flow2 = rrl.AudioSignalFlow( myFirstComposite )
+## TODO: Add checking methods to composite components.
+#
+## Do not attempt this unless the 
+#flow2 = rrl.AudioSignalFlow( myFirstComposite )
 
