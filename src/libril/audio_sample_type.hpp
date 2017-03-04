c55b87496a39c48668c03e2cce4aef4e68ea31e5
Andreas <a.franck@soton.ac.uk>
2017-03-04 21:41:44 +0000

    Added initialisation macro for long double, added documentation.

--- src/libril/audio_sample_type.hpp
+++ src/libril/audio_sample_type.hpp
@@ -15,14 +15,14 @@ template<> struct IdToType<IdParameter> { using Type = TypeParameter; };
 namespace visr
 {
 
-/**
- * Maybe move that here:
- * Definition for the default sample type.
- */
-// using SampleType = float;
-
-struct AudioSampleType
+namespace AudioSampleType
 {
+  /**
+   * Enumeration for the different sample types.
+   * Can be extended, but the type must be registered with the
+   * VISR_AUDIO_SAMPLE_TYPE_DEFINITION macro and in the typeSize() 
+   * function in the implementation file.
+   */
   enum Id
   {
     floatId = 0,
@@ -36,17 +36,37 @@ struct AudioSampleType
     int32Id = 8
   };
 
+  /**
+   * Compile-time translation from type id to type.
+   * Usage: AudioSampleType::IdToType<id>::Type
+   */
   template< AudioSampleType::Id id > struct IdToType {};
+
+  /**
+   * Compile-time translation from sample type to type id.
+   * Usage: AudioSampleType::IdToType<Type>::TypeToId::id
+   */
   template< typename > struct TypeToId {};
 
+  /**
+   * Return the element size for a type id at runtime.
+   */
+  std::size_t typeSize( Id id );
+
+  /**
+   * Register the translations for the Type<-> conversion macros.
+   */
+  //@{
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( float, floatId );
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( double, doubleId );
+  VISR_AUDIO_SAMPLE_TYPE_DEFINITION( long double, longDoubleId );
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( int8_t, int8Id );
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( uint8_t, uint8Id );
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( int16_t, int16Id );
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( uint16_t, uint16Id );
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( int32_t, int32Id );
   VISR_AUDIO_SAMPLE_TYPE_DEFINITION( uint32_t, uint32Id );
+  //@}
 };
 
 } // namespace visr
