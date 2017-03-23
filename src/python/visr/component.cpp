e629c1f2f28302abaddabc8d3f85c722a6d419dd
Andreas Franck <a.franck@soton.ac.uk>
2017-03-23 15:19:41 +0000

    Added 'status' interface and the corresponding error/information categories to ril::Component.

--- src/python/visr/component.cpp
+++ src/python/visr/component.cpp
@@ -8,6 +8,7 @@
 #include <libril/composite_component.hpp>
 #include <libril/parameter_port_base.hpp>
 #include <libril/signal_flow_context.hpp>
+#include <libril/status_message.hpp>
 
 #include <libvisr_impl/component_implementation.hpp>
 
@@ -30,6 +31,13 @@ namespace visr
 
 void exportComponent( pybind11::module& m )
 {
+  pybind11::enum_<::visr::StatusMessage::Kind>( m, "StatusMessage" )
+    .value( "Information", ::visr::StatusMessage::Information )
+    .value( "Warning", ::visr::StatusMessage::Warning )
+    .value( "Error", ::visr::StatusMessage::Error )
+    .value( "Critical", ::visr::StatusMessage::Critical )
+    ;
+
   pybind11::class_<Component>( m, "Component", pybind11::metaclass() ) 
     // Note: 'metaclass' required for static properties
     .def( pybind11::init<SignalFlowContext &, char const*, CompositeComponent *>(),
@@ -43,6 +51,7 @@ void exportComponent( pybind11::module& m )
     .def( "isTopLevel", &Component::isTopLevel )
     .def( "audioPort", static_cast<AudioPortBase&(Component::*)(std::string const &)>(&Component::audioPort), pybind11::arg("portName"), "Return an audio port object by name", pybind11::return_value_policy::reference )
     .def( "parameterPort", static_cast<ParameterPortBase&(Component::*)(std::string const &)>(&Component::parameterPort), pybind11::arg( "portName" ), "Return a parameter port object by name", pybind11::return_value_policy::reference )
+    .def( "status", static_cast<void(Component::*)(::visr::StatusMessage::Kind, char const*)>(&Component::status), pybind11::arg("statusId"), pybind11::arg("message") )
     ;
 }
 
