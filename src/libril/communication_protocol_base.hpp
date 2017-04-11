3c9d5640393c236dcea899923e7d0feb52be484c
Andreas Franck <a.franck@soton.ac.uk>
2017-04-11 15:08:57 +0100

    Moved definition of CommunicationProcolBase::{In|Out}put outside main class definition, work on documentation.

--- src/libril/communication_protocol_base.hpp
+++ src/libril/communication_protocol_base.hpp
@@ -27,7 +27,7 @@ public:
 
   /**
    * Destructor, virtual.
-   * Communication protocols are instantiated and used polymophically, so the desctructor needs to be virtual.
+   * Communication protocols are instantiated and used polymophically, so the destructor needs to be virtual.
    */
   virtual  VISR_CORE_LIBRARY_SYMBOL ~CommunicationProtocolBase();
 
@@ -44,7 +44,7 @@ public:
   virtual ParameterType parameterType() const = 0;
 
   /**
-   * Connect an input port to this protocol.
+   * Connect a protocol input to this protocol.
    * Derived protocol types must override this pure virtual interface.
    * @param port An parameter input with compatible protocol and parameter types.
    * @throw std::exception If the protocol or parameter types do not match.
@@ -54,7 +54,7 @@ public:
   virtual void connectInput( Input* port ) = 0;
 
   /**
-   * Connect an output port to this protocol.
+   * Connect a protocol output to this protocol.
    * Derived protocol types must override this pure virtual interface.
    * @param port An parameter output with compatible protocol and parameter types.
    * @throw std::exception If the protocol or parameter types do not match.
@@ -64,35 +64,79 @@ public:
   virtual void connectOutput( Output* port ) = 0;
 
   /**
+   * Disconnect a protocol input from this protocol.
+   * @return true if the disconnect was successful, i.e., if the protocol port was actually connected,
+   * false otherwise.
    */
   virtual bool disconnectInput( Input* port ) noexcept = 0;
 
+  /**
+  * Disconnect a protocol output from this protocol.
+  * @return true if the disconnect was successful, i.e., if the protocol port was actually connected,
+  * false otherwise.
+  */
   virtual bool disconnectOutput( Output* port ) noexcept= 0;
+};
 
-  class Input
-  {
-  public:
-    virtual VISR_CORE_LIBRARY_SYMBOL ~Input();
+/**
+ * Abstract interface for an input to a protocol.
+ * Communication protocols derived from CommunicationProtocolBase must define classes derived from this base interface,
+ * implementing its pure virtual methods
+ */
+class CommunicationProtocolBase::Input
+{
+public:
+  /**
+   * Virtual destructor.
+   * This class is intended to be instantiated and used polymorphically.
+   */
+  virtual VISR_CORE_LIBRARY_SYMBOL ~Input();
 
-    virtual void setProtocolInstance( CommunicationProtocolBase * protocol ) = 0;
+  /**
+   * Connect the input to protocol instance.
+   */
+  virtual void setProtocolInstance( CommunicationProtocolBase * protocol ) = 0;
 
-    virtual CommunicationProtocolBase * getProtocol() = 0;
+  /**
+   * Return a pointer to the connected protocol, or \p nullptr if it is not connected.
+   */
+  virtual CommunicationProtocolBase * getProtocol() = 0;
 
-    virtual CommunicationProtocolBase const * getProtocol() const = 0;
-  };
+  /**
+  * Return a pointer to the connected protocol, const version. If the input is not connected, return \p nullptr.
+  */
+  virtual CommunicationProtocolBase const * getProtocol() const = 0;
+};
 
-  class Output
-  {
-  public:
-    virtual VISR_CORE_LIBRARY_SYMBOL ~Output();
+/**
+ * Abstract base class for communication protocol inputs.
+ * Communication protocols derived from CommunicationProtocolBase must provide a Input class derived from this base 
+ * class and override its pure virtual methods.
+ */
+class CommunicationProtocolBase::Output
+{
+public:
+  /**
+   * Virtual destructor.
+   * Derived classes are instantiated and used polymorphically, therefore the destructor has to be virtual.
+   */
+  virtual VISR_CORE_LIBRARY_SYMBOL ~Output();
 
-    virtual void setProtocolInstance( CommunicationProtocolBase * protocol ) = 0;
+  /**
+   * Set the protocol instance for this protocol input.
+   */
+  virtual void setProtocolInstance( CommunicationProtocolBase * protocol ) = 0;
 
-    virtual CommunicationProtocolBase * getProtocol() = 0;
+  /**
+   * Return a pointer to the connected communication protocol, or \p nullptr if it is not connected.
+   */
+  virtual CommunicationProtocolBase * getProtocol() = 0;
 
-    virtual CommunicationProtocolBase const * getProtocol() const = 0;
-  };
-
+  /**
+  * Return a pointer to the connected communication protocol (const version),
+  * or \p nullptr if it is not connected.
+  */
+  virtual CommunicationProtocolBase const * getProtocol() const = 0;
 };
 
 } // namespace visr
