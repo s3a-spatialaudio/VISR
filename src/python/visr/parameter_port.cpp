c4fd66816c6dcf2c1bbdafa68e0b442fad9bb493
Andreas Franck <a.franck@soton.ac.uk>
2017-04-11 21:02:34 +0100

    Added binding for PolymorphicParameterOutput.
    * Renamed Parameter{Input|Output} -> Parameter{Input|Output}Base
    * Renamed PolyParameterInput -> ParameterInput

--- src/python/visr/parameter_port.cpp
+++ src/python/visr/parameter_port.cpp
@@ -8,6 +8,7 @@
 #include <libril/parameter_port_base.hpp>
 
 #include <libvisr_impl/polymorphic_parameter_input.hpp>
+#include <libvisr_impl/polymorphic_parameter_output.hpp>
 
 
 #ifdef USE_PYBIND11
@@ -43,18 +44,27 @@ void exportParameterPort( pybind11::module & m)
 //    .def( "hasParameterConfig", &ParameterPortBase::hasParameterConfig)
     ;
 
-  pybind11::class_<ParameterInputBase, ParameterPortBase>( m, "ParameterInput" )
+  pybind11::class_<ParameterInputBase, ParameterPortBase>( m, "ParameterInputBase" )
     .def( "protocolInput", &ParameterInputBase::protocolInput, pybind11::return_value_policy::reference )
     ;
 
-  pybind11::class_<ParameterOutputBase, ParameterPortBase>( m, "ParameterOutput" )
+  pybind11::class_<ParameterOutputBase, ParameterPortBase>( m, "ParameterOutputBase" )
     .def( "protocolOutput", &ParameterOutputBase::protocolOutput, pybind11::return_value_policy::reference )
     ;
 
-  pybind11::class_<PolymorphicParameterInput, ParameterInputBase>( m, "PolyParameterInput" )
+  pybind11::class_<PolymorphicParameterInput, ParameterInputBase>( m, "ParameterInput" )
     .def( pybind11::init<char const *, Component &, ParameterType const &, CommunicationProtocolType const &, ParameterConfigBase const &>(),
       pybind11::arg("name"), pybind11::arg("parent"), pybind11::arg( "parameterType" ),
       pybind11::arg("protocolType"), pybind11::arg("parameterConfig" ) )
+    .def( pybind11::init<char const *, Component &, ParameterType const &, CommunicationProtocolType const &>(),
+      pybind11::arg( "name" ), pybind11::arg( "parent" ), pybind11::arg( "parameterType" ),
+      pybind11::arg( "protocolType" ) )
+    ;
+
+  pybind11::class_<PolymorphicParameterOutput, ParameterOutputBase>( m, "ParameterOutput" )
+    .def( pybind11::init<char const *, Component &, ParameterType const &, CommunicationProtocolType const &, ParameterConfigBase const &>(),
+      pybind11::arg( "name" ), pybind11::arg( "parent" ), pybind11::arg( "parameterType" ),
+      pybind11::arg( "protocolType" ), pybind11::arg( "parameterConfig" ) )
     .def( pybind11::init<char const *, Component &, ParameterType const &, CommunicationProtocolType const &>(),
       pybind11::arg( "name" ), pybind11::arg( "parent" ), pybind11::arg( "parameterType" ),
       pybind11::arg( "protocolType" ) )
