00a5348cad318650aaa19258430fc40973104e1d
Andreas <a.franck@soton.ac.uk>
2017-05-05 16:36:29 +0100

    Added realtime application to run Python components (name needs substantial improvement).

---
+++ src/apps/python_flow_runner/options.cpp
@@ -0,0 +1,37 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include "options.hpp"
+
+#include <cstdint>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+
+namespace visr
+{
+namespace apps
+{
+namespace pythonflowrunner
+{
+
+Options::Options()
+{
+  registerOption<bool>( "help,h", "Show help and usage information." );
+  registerOption<bool>( "version,v", "Display version information." );
+
+  registerOption<std::string>( "audio-backend,D", "The audio backend." );
+  registerOption<std::size_t>( "sampling-frequency,f", "Sampling frequency [Hz]" );
+  registerOption<std::size_t>( "period,p", "Period (blocklength) [Number of samples per audio block]" );
+
+  registerOption<std::string>( "module-path,m", "Full pathe of the Python module to be loaded." );
+  registerOption<std::string>( "python-class-name,c", "Name of the Python class (must be a subclass of visr.Component)." );
+  registerOption<std::string>( "object-name,n", "Name of the Python class (must be a subclass of visr.Component)." );
+  registerOption<std::string>( "positional-arguments,p", "Comma-separated list of positional options passed to the class constructor." );
+  registerOption<std::string>( "keyword-arguments,k", "Comma-separated list of named (keyword) options passed to the class constructor." );
+}
+
+Options::~Options() = default;
+
+} // namespace pythonflowrunner
+} // namespace apps
+} // namespace visr
