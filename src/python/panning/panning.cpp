d64eb2c79a5dd54332611cb53cf68f9690d6a0c2
Andreas <a.franck@soton.ac.uk>
2017-05-08 14:03:33 +0100

    Created Python module for binding panning stuff.
    * Moved LoudspeakerArray here from pml (which was a hack)
    * TODO: Create bindings for other objects (Vpap, AllRAD ?)

---
+++ src/python/panning/panning.cpp
@@ -0,0 +1,28 @@
+/* Copyright Institute of Sound and Vibration Research - All rights reserved */
+
+#include <pybind11/pybind11.h>
+
+// Function declarations
+namespace visr
+{
+namespace python
+{
+namespace panning
+{
+void exportLoudspeakerArray( pybind11::module & m );
+}
+}
+}
+
+PYBIND11_PLUGIN( panning )
+{
+  pybind11::module::import( "visr" );
+
+  pybind11::module m( "panning", "VISR panning data structures and core algorithms" );
+  using namespace visr::python::panning;
+
+  // Export the communication protocols
+  exportLoudspeakerArray( m );
+
+  return m.ptr();
+}
