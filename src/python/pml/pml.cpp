79caafb0d02d55a7b809771d5d775d4473caccd0
Andreas <a.franck@soton.ac.uk>
2017-05-10 15:26:27 +0100

    Added bindings for IndexedValueParameter.

--- src/python/pml/pml.cpp
+++ src/python/pml/pml.cpp
@@ -4,7 +4,7 @@
 #include <libpml/initialise_parameter_library.hpp>
 
 #include "double_buffering_protocol.hpp"
-#include "empty_parameter_config.hpp"
+// #include "empty_parameter_config.hpp"
 #include "listener_position.hpp"
 #include "matrix_parameter.hpp"
 #include "message_queue_protocol.hpp"
@@ -12,6 +12,19 @@
 #include "vector_parameter.hpp"
 
 #include <pybind11/pybind11.h>
+
+namespace visr
+{
+namespace python
+{
+namespace pml
+{
+void exportEmptyParameterConfig( pybind11::module & m );
+void exportIndexedValueParameters( pybind11::module & m );
+}
+}
+}
+
 
 PYBIND11_PLUGIN( pml )
 {
@@ -28,6 +41,7 @@ PYBIND11_PLUGIN( pml )
   exportMessageQueueProtocol( m );
 
   exportEmptyParameterConfig( m );
+  exportIndexedValueParameters( m );
   exportListenerPosition( m );
   exportMatrixParameters( m);
   exportObjectVector( m );
