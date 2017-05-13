5c457c4e6f7dcd6e08ec79af84cd2599265d56eb
Andreas <a.franck@soton.ac.uk>
2017-05-13 11:38:10 +0100

    Added 'channels' property for easier, more Pythonic access to the channel interface.

--- src/python/objectmodel/object.cpp
+++ src/python/objectmodel/object.cpp
@@ -3,6 +3,9 @@
 #include <libobjectmodel/object.hpp>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <vector>
 
 namespace py = pybind11;
 
@@ -15,6 +18,33 @@ namespace python
 
 // TODO: Do we want add a trampoline class in order to enable derivation of new object types in Python?
 
+
+namespace // unnamed
+{
+
+std::vector<Object::ChannelIndex> getChannels( Object const & obj )
+{
+  std::size_t const numChannels = obj.numberOfChannels();
+  std::vector<Object::ChannelIndex> channels( numChannels );
+  for( std::size_t chIdx(0); chIdx < numChannels; ++chIdx )
+  {
+    channels[chIdx] = obj.channelIndex( chIdx );
+  }
+  return channels;
+}
+
+void setChannels( Object& obj, std::vector<Object::ChannelIndex> const channels )
+{
+  std::size_t const numChannels = channels.size();
+  obj.resetNumberOfChannels( numChannels );
+  for( std::size_t chIdx(0); chIdx < numChannels; ++chIdx )
+  {
+    obj.setChannelIndex( chIdx, channels[chIdx] );
+  }
+}
+
+} // unnamed namespace
+
 void exportObject( pybind11::module & m )
 {
   py::class_<Object>( m, "Object" )
@@ -25,6 +55,7 @@ void exportObject( pybind11::module & m )
     .def_property( "level", &Object::level, &Object::setLevel )
     .def_property( "priority", &Object::priority, &Object::setPriority )
     .def_property_readonly( "numberOfChannels", &Object::numberOfChannels )
+    .def_property( "channels", &getChannels, &setChannels )
     .def( "resetNumberOfChannels", &Object::resetNumberOfChannels, py::arg("newChannelNumber") )
     .def( "setChannelIndex", &Object::setChannelIndex, py::arg("index"), py::arg("channelIndex") )
   ;
