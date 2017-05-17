67166a0ddd76074f22ce6a8f283953eead654972
Andreas <a.franck@soton.ac.uk>
2017-05-17 08:22:57 +0100

    Added Intel intrinsics function for vectorMultiplyAddInplace<std::complex<float> >
    * Deactivated by default.

--- src/libefl/vector_functions.cpp
+++ src/libefl/vector_functions.cpp
@@ -22,17 +22,17 @@ namespace efl
 #ifdef USE_INTEL_INTRINSICS
 void getCpuId()
 {
-  const int functionId = 0x01;
-#if 1
-  int eax, ebx, ecx, edx;
-  __cpuid(functionId, eax, ebx, ecx, edx );
-#else
-  int id[4];
-
-  __cpuid( id, functionId );
-
-  std::cout << "CPU features: " << id << std::endl;
-#endif
+//  const int functionId = 0x01;
+//#if 0
+//  int eax, ebx, ecx, edx;
+//  __cpuid(functionId, eax, ebx, ecx, edx );
+//#else
+//  int id[4];
+//
+//  __cpuid( id, functionId );
+//
+//  std::cout << "CPU features: " << id << std::endl;
+//#endif
 }
 #endif
 
@@ -684,7 +684,124 @@ ErrorCode vectorMultiplyAddInplace<double>( double const * const factor1,
 #else
 template ErrorCode vectorMultiplyAddInplace( double const * const, double const * const, double * const, std::size_t, std::size_t );
 #endif
+
+#if USE_INTEL_INTRINSICS
+//template<> ErrorCode vectorMultiply( std::complex<float> const * const factor1, std::complex<float> const * const factor2, std::complex<float> * const result, std::size_t numElements, std::size_t alignment )
+template<> ErrorCode vectorMultiplyAddInplace( std::complex<float> const * const factor1,
+                                               std::complex<float> const * const factor2,
+                                               std::complex<float> * const accumulator,
+                                               std::size_t numElements,
+                                               std::size_t alignment )
+{
+  if( not checkAlignment( factor1, alignment ) ) return alignmentError;
+  if( not checkAlignment( factor2, alignment ) ) return alignmentError;
+  if( not checkAlignment( accumulator, alignment ) ) return alignmentError;
+
+  float const * pf1 = reinterpret_cast<float const *>(factor1);
+  float const * pf2 = reinterpret_cast<float const *>(factor2);
+  float * pAcc = reinterpret_cast<float *>(accumulator);
+
+  std::size_t countN = numElements;
+  if( (alignment >= 4) )
+  {
+    while( countN >= 4 )
+    {
+      __m256 a = _mm256_load_ps( pf1 );
+      __m256 b = _mm256_load_ps( pf2 );
+      __m256 aHigh = _mm256_permute_ps( a, 0xA0 );
+      a = _mm256_permute_ps( a, 0xF5 /*0b11110101*/ );
+      __m256 partRes1 = _mm256_mul_ps( b, aHigh );
+      b = _mm256_permute_ps( b, 0xB1 /*0b10110001*/ );
+      __m256 partRes2 = _mm256_mul_ps( b, a );
+      __m256 acc = _mm256_load_ps( pAcc );
+      __m256 res = _mm256_addsub_ps( partRes1, partRes2 );
+      __m256 finalRes = _mm256_add_ps( res, acc );
+      _mm256_store_ps( pAcc, finalRes );
+
+      countN -= 4;
+      pf1 += 8;
+      pf2 += 8;
+      pAcc += 8;
+    }
+  }
+  else
+  {
+    while( countN >= 4 ) // Same as above, but unaligned memory accesses.
+    {
+      __m256 a = _mm256_loadu_ps( pf1 );
+      __m256 b = _mm256_loadu_ps( pf2 );
+      __m256 aHigh = _mm256_permute_ps( a, 0xA0 );
+      a = _mm256_permute_ps( a, 0xF5 /*0b11110101*/ );
+      __m256 partRes1 = _mm256_mul_ps( b, aHigh );
+      b = _mm256_permute_ps( b, 0xB1 /*0b10110001*/ );
+      __m256 partRes2 = _mm256_mul_ps( b, a );
+      __m256 acc = _mm256_loadu_ps( pAcc );
+      __m256 res = _mm256_addsub_ps( partRes1, partRes2 );
+      __m256 finalRes = _mm256_add_ps( res, acc );
+      _mm256_storeu_ps( pAcc, finalRes );
+      countN -= 4;
+      pf1 += 8;
+      pf2 += 8;
+      pAcc += 8;
+    }
+  }
+  if( countN == 0 ) // Shorthand exit if #elements is a multiple of 4.
+  {
+    return noError;
+  }
+  if( (countN >= 2) and( alignment >= 2 ) ) // Otherwise fall through to the scalar case.
+  {
+    while( countN >= 2 )
+    {
+      __m128 a = _mm_load_ps( pf1 );
+      __m128 b = _mm_load_ps( pf2 );
+      __m128 aHigh = _mm_permute_ps( a, 0xA0 );
+      a = _mm_permute_ps( a, 0xF5 /*0b11110101*/ );
+      __m128 partRes1 = _mm_mul_ps( b, aHigh );
+      b = _mm_permute_ps( b, 0xB1 /*0b10110001*/ );
+      __m128 acc = _mm128_load_ps( pAcc );
+      __m128 partRes2 = _mm_mul_ps( b, a );
+      __m128 res = _mm_addsub_ps( partRes1, partRes2 );
+      __m128 finalRes = _mm_add_ps( acc, res )
+      _mm_store_ps( pAcc, finalRes );
+      countN -= 2;
+      pf1 += 4;
+      pf2 += 4;
+      pAcc += 4;
+    }
+  }
+  if( countN > 0 ) // Scalar code for remaining samples
+  {
+    while( countN > 0 ) // no alignment or remaining samples
+    {
+      __m128 cReal = _mm_set_ps1( pf1[0] );
+      __m128 cImag = _mm_set_ps1( pf1[1] );
+      __m64 const * xL = reinterpret_cast<__m64 const*>(pf2);
+      __m64 * yL = reinterpret_cast<__m64*>(pAcc);
+
+      __m128 ab = _mm_loadl_pi( cReal, xL ); // cReal is a dummy, we don't use the upper half of the register.
+      __m128 acbc = _mm_mul_ps( cReal, ab );
+      __m128 ba = _mm_shuffle_ps( ab, ab, 0xB1 );
+      __m128 acc = _mm_loadl_pi( cReal, yL );
+      __m128 bdad = _mm_mul_ps( cImag, ba );
+      __m128 res = _mm_addsub_ps( acbc, bdad );
+      __m128 finalRes = _mm_add_ps( res, acc )
+      _mm_storel_pi( yL, finalRes );
+
+      --countN;
+      pf1 += 2;
+      pf2 += 2;
+      pAcc += 2;
+    }
+  }
+  return noError;
+}
+#else
 template ErrorCode vectorMultiplyAddInplace( std::complex<float> const * const, std::complex<float> const * const, std::complex<float> * const, std::size_t, std::size_t );
+#endif
+
+
+
 template ErrorCode vectorMultiplyAddInplace( std::complex<double> const * const, std::complex<double> const * const, std::complex<double> * const, std::size_t, std::size_t );
 
 template<typename T>
