f06fc9a19b76cecd92d20e7c15ccbb730740a66b
Andreas Franck <a.franck@soton.ac.uk>
2017-06-30 21:09:47 +0100

    Disabled distinction between static and dynamic library builds when defining the export symbol.
    * For some reason that is not working on MacOS.
    * TODO: Investigate!

--- src/libobjectmodel/export_symbols.hpp
+++ src/libobjectmodel/export_symbols.hpp
@@ -8,26 +8,25 @@
  *
  */
 
-#ifdef VISR_BUILD_OBJECTMODEL_SHARED_LIBRARY
+// For unknown reasons this definition does not becaome visible here:
+// #ifdef VISR_BUILD_OBJECTMODEL_SHARED_LIBRARY
 #ifdef _MSC_VER // Windows platform
 #ifdef VISR_BUILD_OBJECTMODEL_LIBRARY
 #define VISR_OBJECTMODEL_LIBRARY_SYMBOL __declspec(dllexport)
-#define VISR_OBJECTMODEL_PLUGIN_SYMBOL __declspec(dllimport)
 #else
 #define VISR_OBJECTMODEL_LIBRARY_SYMBOL __declspec(dllimport)
-#endif // #ifdef VISR_BUILD_OBJECTMODEL_LIBRARIES
+#endif // #ifdef VISR_BUILD_OBJECTMODEL_LIBRARY
 #elif defined(__GNUC__)
 
 // Unix platforms (Linux and Mac OS X)
 #define VISR_OBJECTMODEL_LIBRARY_SYMBOL __attribute__((visibility("default"))) 
-#define VISR__OBJECTMODEL_PLUGIN_SYMBOL __attribute__((visibility("default")))
 
 #else
 #error "Platform does not support symbol export."
 #endif // #ifdef _WIN32
 
-#else // #ifdef VISR_BUILD_OBJECTMODEL_SHARED_LIBRARY
-#define VISR_OBJECTMODEL_LIBRARY_SYMBOL // empty
-#endif // #ifdef VISR_BUILD_OBJECTMODEL_SHARED_LIBRARY
+// #else // #ifdef VISR_BUILD_OBJECTMODEL_SHARED_LIBRARY
+// #define VISR_OBJECTMODEL_LIBRARY_SYMBOL // empty
+// #endif // #ifdef VISR_BUILD_OBJECTMODEL_SHARED_LIBRARY
 
 #endif // #ifndef VISR_OBJECTMODEL_EXPORT_SYMBOLS_HPP_INCLUDED
