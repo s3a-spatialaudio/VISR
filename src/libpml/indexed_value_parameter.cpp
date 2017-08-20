1771c7e87dc9cfa21770c10b110fbd99442f47dc
Andreas <a.franck@soton.ac.uk>
2017-08-20 17:48:31 +0100

    Moved method definitions to .cpp file, added explicit instantiations.
    * This is done to solved ptoblems with Windows shared libraries.
    * It's a good idea anyway to encapsulated the template instantiations in this library.
    * TODO: Consider moving the definitions into an "implementation .hpp" file.

--- src/libpml/indexed_value_parameter.cpp
+++ src/libpml/indexed_value_parameter.cpp
@@ -7,7 +7,38 @@ namespace visr
 namespace pml
 {
 
-// explicit instantiations here.
+// TODO: Move into seperate header file (_impl.hpp or _inline.hpp)
+template<typename IndexType, typename ValueType >
+IndexedValueParameter< IndexType, ValueType >::IndexedValueParameter()
+ : mData( IndexType(), ValueType() )
+{
+}
+
+template<typename IndexType, typename ValueType >
+IndexedValueParameter< IndexType, ValueType >::IndexedValueParameter( IndexType const & index, ValueType const & value )
+ : mData( index, value )
+{
+}
+
+template<typename IndexType, typename ValueType >
+IndexedValueParameter< IndexType, ValueType >::IndexedValueParameter( ParameterConfigBase const & config )
+ : IndexedValueParameter( dynamic_cast<EmptyParameterConfig const &>(config) )
+{
+}
+
+template<typename IndexedType, typename ValueType >
+IndexedValueParameter< IndexedType, ValueType >::IndexedValueParameter( const EmptyParameterConfig & config )
+ : IndexedValueParameter() // call default constructor.
+{
+}
+
+template<typename IndexedType, typename ValueType >
+IndexedValueParameter< IndexedType, ValueType >::~IndexedValueParameter() = default;
+
+// explicit instantiations.
+template class visr::pml::IndexedValueParameter<std::size_t, std::vector<double> >;
+template class visr::pml::IndexedValueParameter<std::size_t, std::vector<float> >;
+template class visr::pml::IndexedValueParameter<std::size_t, std::string >;
 
 } // namespace pml
 } // namespace visr
