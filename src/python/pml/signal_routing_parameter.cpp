205beb70d320e009c2f82b62d9dbb056db3f5712
Andreas <a.franck@soton.ac.uk>
2017-08-20 22:40:34 +0100

    Corrected and completed implementation of SignalRoutingParameter binding.

--- src/python/pml/signal_routing_parameter.cpp
+++ src/python/pml/signal_routing_parameter.cpp
@@ -34,103 +34,33 @@ void exportSignalRoutingParameter( py::module & m)
   ;
 
   sigParam
-   .def_poperty_readonly("size", &SignalRoutingParameter::size )
    .def( py::init<>() )
    .def( py::init<std::initializer_list<SignalRoutingParameter::Entry> const &>(), py::arg("initList"))
-////   .def( "__iter__", [](SignalRoutingParameter& sr){ return py::make_iterator(sr.begin(), sr.end()); }, "Return a Python iterator over the contained routings." )
+   .def( "__init__", []( SignalRoutingParameter & inst, std::vector<SignalRoutingParameter::Entry> const & val )
+                     {
+                       new (&inst) SignalRoutingParameter();
+                       for( SignalRoutingParameter::Entry const & e : val )
+                       {
+                         inst.addRouting( e );
+                       }
+                     }, py::arg("initList") )
+   .def( "__iter__", [](SignalRoutingParameter& sr){ return py::make_iterator(sr.begin(), sr.end()); }, "Return a Python iterator over the contained routings." )
+   .def_property_readonly("size", &SignalRoutingParameter::size )
    .def( "swap", &SignalRoutingParameter::swap )
 //   .def_poperty_readonly("empty", &SignalRoutingParameter::empty )
    .def( "addRouting", static_cast<void(SignalRoutingParameter::*)(SignalRoutingParameter::IndexType, SignalRoutingParameter::IndexType)>(&SignalRoutingParameter::addRouting),
          py::arg("input"), py::arg("output") )
-//#if 0
-//  /**
-//   * Add a routing entry for the (input, output) pair contained in the entry.
-//   * An existing routing entry for the output index is deleted.
-//   */
-//  void addRouting( Entry const & newEntry );
-//
-//  /**
-//   * Remove a routing entry consisting of a input and an output index.
-//   * @return If a routing for this (input, output) pair existed before, false if not.
-//   */
-//  bool removeEntry( Entry const & entry );
-//
-//  /**
-//   * Remove a routing for a given output index.
-//   * @return True if there was a routing for that output, false if no such entry existed.
-//   */
-//  bool removeEntry( IndexType outputIdx );
-//
-//  /**
-//   * Clear all routing entries.
-//   */
-//#endif
-//         .def( "clear", , &SignalRoutingParameter::clear );
-//         .def( "getEntry", &SignalRoutingParameter::getEntry );
-//         .def( "getInput", &SignalRoutingParameter::getIn;
-//         .def( "getOutput", &SignalRoutingParameter::getOutput;
-//         .def( "parse", &SignalRoutingParameter::parse, py::arg("encoded")  );
-  ;
- ;
-#if 0
-  void swap( SignalRoutingParameter& rhs );
-
-  bool empty() const { return mRoutings.empty(); }
-
-  std::size_t size() const { return mRoutings.size(); }
-
-  SignalRoutingParameter & operator=(SignalRoutingParameter const & rhs) = default;
-
-  RoutingsType::const_iterator begin() const { return mRoutings.begin(); }
-
-  RoutingsType::const_iterator end() const { return mRoutings.end(); }
-
-  void addRouting( IndexType inputIdx, IndexType outputIdx )
-  {
-    addRouting( Entry{ inputIdx, outputIdx } );
-  }
-
-  /**
-   * Add a routing entry for the (input, output) pair contained in the entry.
-   * An existing routing entry for the output index is deleted.
-   */
-  void addRouting( Entry const & newEntry );
-
-  /**
-   * Remove a routing entry consisting of a input and an output index.
-   * @return If a routing for this (input, output) pair existed before, false if not.
-   */
-  bool removeEntry( Entry const & entry );
-
-  /**
-   * Remove a routing for a given output index.
-   * @return True if there was a routing for that output, false if no such entry existed.
-   */
-  bool removeEntry( IndexType outputIdx );
-
-  /**
-   * Clear all routing entries.
-   */
-  void clear();
-
-  Entry const & getEntry( IndexType outputIdx ) const
-  {
-    static const Entry returnInvalid{ cInvalidIndex, cInvalidIndex };
-
-    RoutingsType::const_iterator const findIt = mRoutings.find( Entry{ cInvalidIndex, outputIdx } );
-    return findIt == mRoutings.end() ? returnInvalid : *findIt;
-  }
-
-  IndexType getInput( IndexType outputIdx ) const
-  {
-    RoutingsType::const_iterator const findIt = mRoutings.find( Entry{ cInvalidIndex, outputIdx } );
-    return findIt == mRoutings.end() ? cInvalidIndex : findIt->input;
-  }
-
-  IndexType getOutput( IndexType inputIdx ) const;
-
-  bool parse( std::string const & encoded );
-#endif
+   .def( "addRouting", static_cast<void(SignalRoutingParameter::*)(SignalRoutingParameter::Entry const &)>(&SignalRoutingParameter::addRouting),
+         py::arg("entry"))
+   .def( "removeRouting", static_cast<bool(SignalRoutingParameter::*)(SignalRoutingParameter::Entry const &)>(&SignalRoutingParameter::removeEntry),
+          py::arg("entry"))
+   .def( "removeRouting", static_cast<bool(SignalRoutingParameter::*)(SignalRoutingParameter::IndexType)>(&SignalRoutingParameter::removeEntry),
+         py::arg("outputIndex"))
+   .def( "clear", &SignalRoutingParameter::clear )
+   .def( "getEntry", &SignalRoutingParameter::getEntry )
+   .def( "getInput", &SignalRoutingParameter::getInput )
+   .def( "getOutput", &SignalRoutingParameter::getOutput )
+   .def( "parse", &SignalRoutingParameter::parse, py::arg("encoded")  )
   ;
 }
 
