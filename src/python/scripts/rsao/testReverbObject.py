774560e3282608a28265ec05b0c508727aaaee31
Andreas Franck <a.franck@soton.ac.uk>
2017-08-29 12:19:01 +0100

    Fixed test script for reverb object rendering, now produces sensible result

--- src/python/scripts/rsao/testReverbObject.py
+++ src/python/scripts/rsao/testReverbObject.py
@@ -5,8 +5,8 @@ Created on Tue Feb 14 15:59:11 2017
 @author: af5u13
 """
 
-#exec(open("/home/andi/dev/visr/src/python/scripts/rsao/testReverbObject.py").read())
-
+# Usage for bebugging from raw Python console
+#exec(open("/Users/af5u13/dev/visr/src/python/scripts/rsao/testReverbObject.py").read())
 
 import visr
 import signalflows
@@ -18,7 +18,6 @@ import objectmodel as om
 import numpy as np;
 import matplotlib.pyplot as plt
 import os
-import scipy.io.wavfile as wavio
 
 # Get VISR base directory from rsao subdirectory.
 visrBaseDirectory = os.path.join( os.getcwd(), '../../../..' )
@@ -34,6 +33,9 @@ samplingFrequency = 48000
 parameterUpdatePeriod = 1024
 
 numBlocks = 64;
+
+numChannels = 5;
+
 
 signalLength = blockSize * numBlocks
 t = 1.0/samplingFrequency * np.arange(0,signalLength)
@@ -53,12 +55,15 @@ diffFiltersRaw = np.array(pml.MatrixParameterFloat.fromAudioFile( diffFilterFile
                           dtype = np.float32 )
 diffFilters = pml.BasicMatrixFloat( diffFiltersRaw[ np.array(lc.channelIndices() )-1,: ] )
 
+reverbConfigStr = '{ "numReverbObjects": %i, "discreteReflectionsPerObject": 20, "lateReverbFilterLength": 2.0, "lateReverbDecorrelationFilters": "%s/config/filters/random_phase_allpass_64ch_1024taps.wav" }' % (numChannels, visrBaseDirectory )
+
 renderer1 = signalflows.CoreRenderer( ctxt, 'renderer1', None,
                                       loudspeakerConfiguration=lc,
                                       numberOfInputs=1,
                                       numberOfOutputs=numOutputChannels, 
                                       interpolationPeriod=parameterUpdatePeriod, 
                                       diffusionFilters=diffFilters,
+                                      reverbConfig=reverbConfigStr,
                                       trackingConfiguration='' )
 
 
@@ -71,20 +76,21 @@ jsonFile = os.path.join( visrBaseDirectory, 'src/libobjectmodel/test/data/point_
 obj_vector=om.ObjectVector() # load the object vector
 obj_str=open(jsonFile).read() # read json object vector as a string
 obj_vector.fillFromJson(obj_str) # populate the object vector
+ro = obj_vector[0]
                        
 paramInput = flow.parameterReceivePort('objectDataInput')
 
 inputSignal = np.zeros( (1, signalLength ), dtype=np.float32 )
-inputSignal[0,:] = 0.75*np.sin( 2.0*np.pi*440 * t )
-
+# inputSignal[0,:] = 0.75*np.sin( 2.0*np.pi*440 * t )
+inputSignal[ 0, 100 ] = 1
 
 outputSignal = np.zeros( (numOutputChannels, signalLength ), dtype=np.float32 )
 
 for blockIdx in range(0,numBlocks):
     if blockIdx % (parameterUpdatePeriod/blockSize) == 0:
-
         ov = paramInput.data()
-        ov = obj_vector
+        ov.clear()
+        ov.set(  ro.objectId, ro )
         paramInput.swapBuffers()
         
     inputBlock = inputSignal[:, blockIdx*blockSize:(blockIdx+1)*blockSize]
@@ -93,5 +99,5 @@ for blockIdx in range(0,numBlocks):
 
 
 plt.figure(1)
-plt.plot( t, outputSignal[12,:], 'bo-', t, outputSignal[13,:], 'rx-' )
-plt.show()
+plt.plot( t, outputSignal[2,:], 'bo-', t, outputSignal[6,:], 'rx-',  t, outputSignal[21,:], 'm.-' )
+plt.show( block = False )
