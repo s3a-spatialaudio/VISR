f921ee19def061b9530f30b37a587b6c22bb98b5
gc1y17 <g.costantini@soton.ac.uk>
2017-09-15 19:11:47 +0100

    missed rotationFunctions

---
+++ src/python/scripts/binaural/rotationFunctions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 15 16:50:56 2017
+
+@author: gc1y17
+"""
+import math
+import numpy as np
+
+idMatrix = np.identity(3)
+
+def calcRotationMatrix(ypr):  
+  if ypr.size == 3 :
+   # print(type(ypr))
+    psi = np.float32(ypr.item(0))
+   # print(type(psi))
+    the = np.float32(ypr.item(1))
+   # print(type(the))
+    phi = np.float32(ypr.item(2))
+
+
+    a11 = math.cos(the) * math.cos(phi)
+    a12 = np.math.cos(the) * np.math.sin(phi)
+    a13 = -np.math.sin(the)
+    
+    a21 = np.math.sin(psi) * np.math.sin(the) * np.math.cos(phi) - np.math.cos(psi) * np.math.sin(phi)
+    a22 = np.math.sin(psi) * np.math.sin(the) * np.math.sin(phi) + np.math.cos(psi) * np.math.cos(phi)
+    a23 = np.math.cos(the) * np.math.sin(psi)
+
+    a31 = np.math.cos(psi) * np.math.sin(the) * np.math.cos(phi) + np.math.sin(psi) * np.math.sin(phi)
+    a32 = np.math.cos(psi) * np.math.sin(the) * np.math.sin(phi) - np.math.sin(psi) * np.math.cos(phi)
+    a33 = np.math.cos(the) * np.math.cos(psi)
+    
+    rotation = np.matrix([[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]])
+    return rotation
+  else:
+    return idMatrix
+ 
