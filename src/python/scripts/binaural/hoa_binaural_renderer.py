Fixed HOA binaural synthesis, rotation functions still need to be inserted into HoaCoefficientRotation.

src/python/scripts/binaural/hoa_binaural_renderer.py
@@ -58,14 +58,14 @@ class HoaBinauralRenderer( visr.CompositeComponent ):
                                              interpolationSteps = interpolationSteps,
                                              initialGains = 0.0,
                                              controlInput = True )
-        self.audioConnection( self.objectSignalInput, self.encoderMatrix.audioport("in") )
+        self.audioConnection( self.objectSignalInput, self.encoderMatrix.audioPort("in") )
 
         filterMtx = np.concatenate( (filters[0:numHoaCoeffs,0,:], filters[0:numHoaCoeffs,1,:]) )
 
         routings = pml.FilterRoutingList()
         for idx in range(0,numHoaCoeffs):
-            routings.addRouting( idx, idx, idx )
-            routings.addRouting( idx, idx+numHoaCoeffs, idx+numHoaCoeffs )
+            routings.addRouting( idx, 0, idx, 1.0 )
+            routings.addRouting( idx, 1, idx+numHoaCoeffs, 1.0 )
 
         self.binauralFilterBank = rcl.FirFilterMatrix( context, 'binauralFilterBank', self,
                                                     numberOfInputs = numHoaCoeffs,
@@ -77,11 +77,11 @@ class HoaBinauralRenderer( visr.CompositeComponent ):
                                                     routings = routings,
                                                     controlInputs=rcl.FirFilterMatrix.ControlPortConfig.NoInputs )
 
-        self.audioConnection( self.encoderMatrix.audioport("out"), self.binauralFilterBank.audioport("in") )
-        self.audioConnection( self.binauralFilterBank.audioport("out"), self.binauralOutput )
+        self.audioConnection( self.encoderMatrix.audioPort("out"), self.binauralFilterBank.audioPort("in") )
+        self.audioConnection( self.binauralFilterBank.audioPort("out"), self.binauralOutput )
 
         if headTracking:
-            self.trackingInput = visr.ParameterInput( "tracking", self, pml.ListenerPosition.staticType,
+            self.trackingInput = visr.ParameterInput( "headTracking", self, pml.ListenerPosition.staticType,
                                               pml.DoubleBufferingProtocol.staticType,
                                               pml.EmptyParameterConfig() )
             self.coefficientRotator = HoaCoefficientRotation( context, 'coefficientRotator', self,

src/python/scripts/binaural/hoa_coefficient_rotation.py
@@ -30,26 +30,20 @@ class HoaCoefficientRotation( visr.AtomicComponent ):
         self.numberOfObjects = numberOfObjects
         self.hoaOrder = hoaOrder
         self.numHoaCoeffs = (self.hoaOrder+1)**2
-        self.coeffMatrix = np.zeros( (self.numberOfObjects, self.numHoaCoeffs), dtype = np.float32 )
         self.rotationMatrix = np.identity( 3, dtype = np.float32 ) # TODO: decide whether to use xyz or ACN (yzx) order
 
         # %% Define parameter ports
-        self.objectInput = visr.ParameterInput( "objectVector", self, pml.ObjectVector.staticType,
-                                              pml.DoubleBufferingProtocol.staticType,
-                                              pml.EmptyParameterConfig() )
-        self.objectInputProtocol = self.objectInput.protocolInput()
-
-        matrixConfig = pml.MatrixParameterConfig(self.numberOfObjects, self.numHoaCoeffs)
+        matrixConfig = pml.MatrixParameterConfig( self.numHoaCoeffs, self.numberOfObjects )
         self.coefficientInput = visr.ParameterInput( "coefficientInput", self,
                                                 pml.MatrixParameterFloat.staticType,
-                                                pml.DoubleBufferingProtocol.staticType,
+                                                pml.SharedDataProtocol.staticType,
                                                 matrixConfig )
-        self.coefficientInputProtocol = self.coeffcientInput.protocolInput()
+        self.coefficientInputProtocol = self.coefficientInput.protocolInput()
         self.coefficientOutput = visr.ParameterOutput( "coefficientOutput", self,
                                                 pml.MatrixParameterFloat.staticType,
-                                                pml.DoubleBufferingProtocol.staticType,
+                                                pml.SharedDataProtocol.staticType,
                                                 matrixConfig )
-        self.coefficientOutputProtocol = self.coeffcientOutput.protocolOutput()
+        self.coefficientOutputProtocol = self.coefficientOutput.protocolOutput()
         
         # Instantiate the head tracker input.
         self.trackingInput = visr.ParameterInput( "headTracking", self, pml.ListenerPosition.staticType,
@@ -58,33 +52,25 @@ class HoaCoefficientRotation( visr.AtomicComponent ):
         self.trackingInputProtocol = self.trackingInput.protocolInput()
 
     def process( self ):
-        recompute = False
-        if self.coefficientInputProtocol.changed():
-            self.coeffMatrix[...] = self.coefficientInputProtocol.data()
-            recompute = True
-            self.coefficientInputProtocol.resetChanged()
 
         if self.trackingInputProtocol.changed():
             head = self.trackingInputProtocol.data()
-            ypr = - head.orientation # negative because we rotate the sound field in the opposite 
+            ypr = - np.array(head.orientation, dtype = np.float32 ) # negative because we rotate the sound field in the opposite 
             # direction of the head orientation.
-            # TODO
             self.rotationMatrix[...] = calcRotationMatrix( ypr )
-            recompute = True
             self.trackingInputProtocol.resetChanged()
 
-        if recompute:
-            coeffOut = np.array( self.coefficientOutputProtocol.data(), copy = False )
-            # Order 0 remains unchanged
-            coeffOut[0,:] = self.coeffMatrix[0,:]
+        coeffIn = np.array( self.coefficientInputProtocol.data(), copy = False )
+        coeffOut = np.array( self.coefficientOutputProtocol.data(), copy = False )
 
-            # TODO: If not done before, translate the rotation matrix to ACN format
+        # Order 0 remains unchanged
+        coeffOut[0,:] = coeffIn[0,:]
 
-            # Compute order order by order
-            for order in range(1, self.hoaOrder+1):
-                # TODO: Compute the transformation matrix for order 'order'
-                rot = np.identity( 2*order + 1, dtype = coeffOut.dtype ) # Dummy rotation matrix
+        # TODO: If not done before, translate the rotation matrix to ACN format
 
-                coeffOut[:,(order**2):((order+1)**2) ] = np.matmul( self.coeffMatrix[:,(order**2):((order+1)**2) ], rot )
+        # Compute order order by order
+        for order in range(1, self.hoaOrder+1):
+            # TODO: Compute the transformation matrix for order 'order'
+            rot = np.identity( 2*order + 1, dtype = coeffOut.dtype ) # Dummy rotation matrix
 
-            self.coefficientOutputProtocol.swapBuffers()
+            coeffOut[ (order**2):((order+1)**2), : ] = np.matmul( rot, coeffIn[ (order**2):((order+1)**2), : ] )

src/python/scripts/binaural/hoa_encoder.py
@@ -11,6 +11,7 @@ Created on Fri 06 Oct 2017
 import visr
 import pml
 import objectmodel as om
+import rbbl
 # import time
 
 from rotationFunctions import cart2sph
@@ -38,14 +39,16 @@ class HoaEncoder( visr.AtomicComponent ):
                                               pml.EmptyParameterConfig() )
         self.objectInputProtocol = self.objectInput.protocolInput()
 
-        matrixConfig = pml.MatrixParameterConfig(self.numberOfObjects, self.numHoaCoeffs)
+        matrixConfig = pml.MatrixParameterConfig( self.numHoaCoeffs, self.numberOfObjects )
         self.coefficientOutput = visr.ParameterOutput( "coefficientOutput", self,
                                                 pml.MatrixParameterFloat.staticType,
-                                                pml.DoubleBufferingProtocol.staticType,
+                                                pml.SharedDataProtocol.staticType,
                                                 matrixConfig )
         self.coefficientOutputProtocol = self.coefficientOutput.protocolOutput()
 
         if channelAllocation:
+            self.channelAllocator = rbbl.ObjectChannelAllocator( self.numberOfObjects )
+            self.usedChannels = set()
             self.routingOutput = visr.ParameterOutput( "routingOutput", self,
                                                      pml.SignalRoutingParameter.staticType,
                                                      pml.DoubleBufferingProtocol.staticType,
@@ -53,13 +56,14 @@ class HoaEncoder( visr.AtomicComponent ):
             self.routingOutputProtocol = self.routingOutput.protocolOutput()
         else:
             self.routingOutputProtocol = None
+            self.channelAllocator = None
 
     def process( self ):
         if self.objectInputProtocol.changed():
             ov = self.objectInputProtocol.data();
             
             coeffOut = np.array( self.coefficientOutputProtocol.data(), copy=False )
-            if coeffOut.shape != (self.numberOfObjects, self.numHoaCoeffs ):
+            if coeffOut.shape != ( self.numHoaCoeffs, self.numberOfObjects ):
                 raise ValueError( 'The dimensions of the output coefficient matrix does not match the expected shape.' )
             coeffOut[...] = 0.0
 
@@ -75,13 +79,10 @@ class HoaEncoder( visr.AtomicComponent ):
                     src = ov[objIdx]
                     sph = cart2sph( src.x, src.y, src.z )
                     pwCoeffs = allSphHarmRealACN( self.hoaOrder, np.pi/2-sph[1], sph[0], dtype = coeffOut.dtype )
-                    coeffOut[chIdx, : ] = pwCoeffs * src.level # encode the object level in the coefficients
+                    coeffOut[ :, chIdx] = pwCoeffs * src.level # encode the object level in the coefficients
             else:
-                self.levels[:] = 0.0
                 for src in ov:
-                    ch = src.channels[0]
+                    chIdx = src.channels[0]
                     sph = cart2sph( src.x, src.y, src.z )
                     pwCoeffs = allSphHarmRealACN( self.hoaOrder, np.pi/2-sph[1], sph[0], dtype = coeffOut.dtype )
-                    coeffOut[ch, : ] = pwCoeffs * src.level # encode the object level in the coefficients
-            
-            self.coefficientOutputProtocol.swapBuffers()
+                    coeffOut[ :, chIdx ] = pwCoeffs * src.level # encode the object level in the coefficients

src/python/scripts/binaural/test_hoa_binaural_renderer.py
@@ -7,7 +7,6 @@ Created on Mon Oct 09 2017
 """
 
 from hoa_binaural_renderer import HoaBinauralRenderer
-from rotationFunctions import sph2cart
 import visr
 import rrl
 import objectmodel
@@ -17,24 +16,35 @@ import matplotlib.pyplot as plt
 
 import os
 
+def sph2cart(az,el,r):
+    x = r*np.cos(az)*np.cos(el)
+    y = r*np.sin(az)*np.cos(el)
+    z = r*np.sin(el)
+    return x,y,z
+
+# switch dynamic tracking on and off.
+useTracking = True
+
+
 fs = 48000
 
 numBinauralObjects = 12
 
-maxHoaOrder = 3
+# datasets are provided for odd orders 1,3,5,7,9
+maxHoaOrder = 9
 
 blockSize = 512
 
 parameterUpdatePeriod = 1
-numBlocks = 1024;
+numBlocks = 128;
 #numBlocks = 64;
 signalLength = blockSize * numBlocks
 t = 1.0/fs * np.arange(0,signalLength)
 numOutputChannels = 2;
-
+#numOutputChannels = (maxHoaOrder+1)**2
 
 currDir = os.getcwd()
-sofaFile = os.path.join( currDir, './data/bbc_hoa2bin_sofa/Gauss_O3_ku100_dualband_energy.sofa' )
+sofaFile = os.path.join( currDir, './data/bbc_hoa2bin_sofa/Gauss_O%d_ku100_dualband_energy.sofa' % maxHoaOrder )
 
 context = visr.SignalFlowContext( period=blockSize, samplingFrequency=fs)
 
@@ -43,7 +53,7 @@ graph = HoaBinauralRenderer( context, "HoaBinauralRenderer", None,
                             maxHoaOrder = maxHoaOrder,
                             sofaFile = sofaFile,
                             interpolationSteps = blockSize,
-                            headTracking = False
+                            headTracking = useTracking
                             )
 
 result,messages = rrl.checkConnectionIntegrity(graph)
@@ -52,43 +62,46 @@ if not result:
 
 flow = rrl.AudioSignalFlow( graph )
 
-paramInput = flow.parameterReceivePort('objectDataInput')
+paramInput = flow.parameterReceivePort('objectVector')
+
+
+if useTracking:
+    trackingInput = flow.parameterReceivePort( 'headTracking' )
 
 inputSignal = np.zeros( (numBinauralObjects, signalLength ), dtype=np.float32 )
-inputSignal[0,100] = 1.0 # Discrete Dirac pulse
+inputSignal[0,:] = 0.75*np.sin( 2.0*np.pi*440 * t )
 
 outputSignal = np.zeros( (numOutputChannels, signalLength ), dtype=np.float32 )
 
-az = 0.025
-el = 0.1 * np.sin( 0.025)   
-r = 1
-x,y,z = sph2cart( az, el, r )
-ps1 = objectmodel.PointSource(0)
-ps1.x = x
-ps1.y = y
-ps1.z = z
-ps1.level = 0.5
-ps1.groupId = 5
-ps1.priority = 5
-ps1.resetNumberOfChannels(1)
-ps1.setChannelIndex(0,ps1.objectId)
-
-ov = paramInput.data()
-ov.clear()
-ov.insert( ps1 )
-paramInput.swapBuffers()
-
 for blockIdx in range(0,numBlocks):
-    
-    az = 0.025 * blockIdx
-    el = 0.1 * np.sin( 0.025 * blockIdx )
-    
+
+    az = 0.1 * blockIdx
+    el = 0.1 * np.sin( 0.05 * blockIdx )
+    x,y,z = sph2cart( az, el, 1.0 )
+    ps1 = objectmodel.PointSource(0)
+    ps1.x = x
+    ps1.y = y
+    ps1.z = z
+    ps1.level = 0.5
+    ps1.groupId = 5
+    ps1.priority = 5
+    ps1.channels = [0]
+    ov = paramInput.data()
+    ov.clear()
+    ov.insert( ps1 )
+    paramInput.swapBuffers()
+
+    if useTracking:
+        yaw = (0.05 * blockIdx) % (2*np.pi)
+        pitch = 0.1 * np.sin( -0.05 * blockIdx )
+        trackingInput.data().orientation = [yaw, pitch, 0]
+        trackingInput.swapBuffers()
+
     inputBlock = inputSignal[:, blockIdx*blockSize:(blockIdx+1)*blockSize]
     #print(str(inputBlock.shape))
     outputBlock = flow.process( inputBlock )
     outputSignal[:, blockIdx*blockSize:(blockIdx+1)*blockSize] = outputBlock
 
-
 plt.figure()
-plt.plot( t, outputSignal[0,:], 'b-',t, outputSignal[1,:], 'r-')
+plt.plot( t, outputSignal[0,:], 'bo-',t, outputSignal[1,:], 'r-')
 plt.show()