63509551b7e3fd19c96baca11ea0cf4418745740
Andreas <a.franck@soton.ac.uk>
2018-02-20 22:47:47 +0000

    Modifications to apps::SceneDecoder after moving setup of rcl::UdpSender ito constructor.

--- src/apps/scene_decoder/signal_flow.cpp
+++ src/apps/scene_decoder/signal_flow.cpp
@@ -34,7 +34,7 @@ SignalFlow::SignalFlow( SignalFlowContext & context,
  , mSceneReceiver( context, "SceneReceiver", this )
  , mSceneDecoder( context, "SceneDecoder", this )
  , mSceneEncoder( context, "SceneEncoder", this )
- , mSceneSender( context, "SceneSender", this )
+ , mSceneSender( context, "SceneSender", this, 9998, "152.78.243.62", 9999, rcl::UdpSender::Mode::Asynchronous )
  , mGainCalculator( context, "VbapGainCalculator", this, 
                     numberOfInputs,
                     panning::LoudspeakerArray( configFile ) )
@@ -51,7 +51,6 @@ SignalFlow::SignalFlow( SignalFlowContext & context,
    mMatrix.setup( cNumberOfInputs, cNumberOfOutputs, cInterpolationSteps, 1.0f );
 
    mSceneEncoder.setup();
-   mSceneSender.setup( 9998, "152.78.243.62", 9999, rcl::UdpSender::Mode::Asynchronous );
 
    audioConnection( "this", "in", ChannelRange( 0, cNumberOfInputs ), "GainMatrix", "in", ChannelRange( 0, cNumberOfInputs ) );
    audioConnection( "GainMatrix", "out", ChannelRange( 0, cNumberOfOutputs ), "this", "out", ChannelRange( 0, cNumberOfOutputs ) );
