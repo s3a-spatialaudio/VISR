7f7cb9794a40ecff60bf864ed6d4e01d4c9652eb
Andreas <a.franck@soton.ac.uk>
2018-08-16 04:58:06 +0100

    Made  visr::ChannelList fit for use in exported targets, cleanup and documentation
    * added export symbol macro,  
    * added  destructor implementation (default) to avoid multiple instantiations. 
    * Completed Doxygen documentation

--- src/libvisr/channel_list.hpp
+++ src/libvisr/channel_list.hpp
@@ -16,21 +16,46 @@ namespace visr
 // Forward declaration
 class ChannelRange;
 
-class ChannelList
-  
+/**
+ * Class representing a list of channel indices.
+ * It is used to specify arbitrary connections and channel routings between audio ports.
+ * @see CompositeComponent::audioConnection
+ */
+class VISR_CORE_LIBRARY_SYMBOL ChannelList
 {
 public:
   using IndexType = std::size_t;
 
-  VISR_CORE_LIBRARY_SYMBOL ChannelList();
+  /**
+   * Default constructor, creates an empty channel list.
+   */
+  ChannelList();
 
   /**
-  * Construct a channel list from a single channel range.
-  * This constructor ius intended to support
+  * Construct a channel list from a single channel range, i.e., from a start, stop, stride description.
+  * This constructor is supposed to support type conversion, for instance to enable the use of a ChannelRange object in a
+  * CompositeComponent::audioConnection() call.
+  * Therefore it is not marked as explicit.
+  * @param range A ChannelRange object defining an equidistantly strided range of channels.
   */
-  VISR_CORE_LIBRARY_SYMBOL ChannelList( ChannelRange const & range );
+  ChannelList( ChannelRange const & range );
 
-  VISR_CORE_LIBRARY_SYMBOL ChannelList( std::initializer_list<IndexType> const & initList );
+  /**
+   * Construct a channel list from an initialiser list.
+   * This constructor is suppoed to be used for type conversion, for instance to enable an initialiser list as argument in a
+   * CompositeComponent::audioConnection call.
+   * Therefore it is not marked as explicit.
+   * @param initList Initaliser list of channels indices, typically a comma,separated sequence enclosed in curly braces.
+   */
+  ChannelList( std::initializer_list<IndexType> const & initList );
+
+  /**
+   * Construct a channel list from a initialiser list of ChannelRange objects.
+   * This constructor is to support implicit type conversion, for instance to use an initialiser list directly in 
+   * a CompositeComponent::audioConnection call.
+   * @param initList Initialiser list of ChannelRange objects, typically enclosed in curly braces.
+   */
+  ChannelList( std::initializer_list<ChannelRange> const & initList );
 
   /**
   * Construct a channel list from a container.
@@ -49,8 +74,6 @@ public:
 
   /**
    * Construct a channel list from a sequence.
-   * @note This template member function is not an export of the shared library because it is 
-   * instantiated at the caller site.
    * @tparam Iterator Iterator type matching the forward iterator concept and whose 
    * referenced type is convertible to the index type.
    * @param begin Begin iterator
@@ -65,33 +88,78 @@ public:
     }
   }
 
+  /**
+   * Destructor.
+   */
+  ~ChannelList();
 
-  VISR_CORE_LIBRARY_SYMBOL ChannelList( std::initializer_list<ChannelRange> const & initList );
+  /**
+   * Return the number of elements in the channel list.
+   */
+  std::size_t size() const;
 
-  VISR_CORE_LIBRARY_SYMBOL std::size_t size() const;
+  /**
+   * Unchecked index access to the channel indices, const version.
+   * Does not check whether idx is within the permissible range.
+   * @param idx Zero-offset position index within the channel list.
+   */
+  IndexType operator[]( std::size_t idx ) const;
 
-  VISR_CORE_LIBRARY_SYMBOL IndexType operator[]( std::size_t idx ) const;
+  /**
+  * Unchecked index access to the channel indices.
+  * Does not check whether idx is within the permissible range.
+  * @param idx Zero-offset position index within the channel list.
+  */
+  IndexType& operator[]( std::size_t idx );
 
-  VISR_CORE_LIBRARY_SYMBOL IndexType& operator[]( std::size_t idx );
+  /**
+   * Bounds-checking index access to the channel indices, const version.
+   * @throw std::out_of_range If \p idx exceeds the number of indices in the list.
+   * @param idx Zero-offset position index within the channel list.
+   */
+  IndexType at( std::size_t idx ) const;
 
-  VISR_CORE_LIBRARY_SYMBOL IndexType at( std::size_t idx ) const;
-
-  VISR_CORE_LIBRARY_SYMBOL IndexType& at( std::size_t idx );
+  /**
+   * Bounds-checking index access to the channel indices.
+   * @throw std::out_of_range If \p idx exceeds the number of indices in the list.
+   * @param idx Zero-offset position index within the channel list.
+   */
+  IndexType& at( std::size_t idx );
 
   using ListType = std::vector<IndexType>;
   using const_iterator = ListType::const_iterator;
 
-  VISR_CORE_LIBRARY_SYMBOL const_iterator begin() const { return mChannels.begin(); }
-  VISR_CORE_LIBRARY_SYMBOL const_iterator end() const { return mChannels.end(); }
+  /**
+   * Return a const iterator to the beginning of the index list.
+   * @todo Check whether we should provide a cbegin() version as well.
+   * @todo Check whether we should provide a non-const version too.
+   */
+  const_iterator begin() const { return mChannels.begin(); }
 
+  /**
+  * Return a const iterator to the end of the index list.
+  * @todo Check whether we should provide a cend() version as well.
+  * @todo Check whether we should provide a non-const version too.
+  */
+  const_iterator end() const { return mChannels.end(); }
 private:
 
-
+  /**
+   * The container for the channels indices.
+   */
   ListType mChannels;
 
-  VISR_CORE_LIBRARY_SYMBOL void appendIndex( IndexType index );
+  /**
+   * Internal function for appending indices to the end of the index list.
+   * Its basic purpose is to move the actual implementation of the templated constructors accepting containers and iterator 
+   * ranges outside the header file.
+   */
+  void appendIndex( IndexType index );
 
-  VISR_CORE_LIBRARY_SYMBOL void appendRange( ChannelRange const & range );
+  /**
+   * Interanl function to append a channel range to the end of the channel list.
+   */
+  void appendRange( ChannelRange const & range );
 };
 
 /**
@@ -101,6 +169,11 @@ private:
  */
 VISR_CORE_LIBRARY_SYMBOL std::ostream & operator<<( std::ostream & str, ChannelList const channels );
 
+/**
+ * Class representing a sequence of equidistant channel indices caharcterised by a start, an end, and step (or stride) value.
+ * The end value is not included in the range following the 'one-past-end' semantics used in the STL or Python.
+ * Strides can be negative but must be non-zero.
+ */
 class VISR_CORE_LIBRARY_SYMBOL ChannelRange
 {
 public:
@@ -115,11 +188,15 @@ public:
   /**
   * Construct a single-element range.
   * @note this constructor is not explicit to enable construction of ChannelList objects from initializer lists.
+  * @param val The start valu (and sole value of the resulting range)
   */
   ChannelRange( IndexType val );
 
   /**
   * Construct a range from start, end, and optional step values
+  * @param start The start value of the sequence.
+  * @param end The end value of the sequence. this value is not included in the index sequence.
+  * @param step The increment between successive channel indices, must be non-zero.
   * @throw std::invalid_argument the range specification is invalid (i.e., if step is zero or the increments goes in the different direction of the end value.)
   * @note this constructor is not explicit to enable construction of ChannelList objects from initializer lists of ranges.
   */
@@ -127,7 +204,11 @@ public:
 
   /**
    * Set a range using start, end, and (optional) step notation
-   * @throw std::invalid_argument the range specification is invalid.
+   * @param start The start value of the sequence.
+   * @param end The end value of the sequence. this value is not included in the index sequence.
+   * @param stride The increment between successive channel indices, must be non-zero.
+   * @throw std::invalid_argument the range specification is invalid (i.e., if step is zero or the increments goes in the different direction of the end value.)
+   * @see isValid
    */
   void set( IndexType start, IndexType end, StepType stride = 1 );
 
@@ -138,17 +219,29 @@ public:
 
   /**
    * Query whether the range is valid, i.e., whether it contains a finite (potentially zero) number of elements.
+   * That is, ranges are invalid if the stride is zero or the end index cannot be reached from the start index with the given stride value.
    */
   bool isValid() const;
 
   /**
-  */
+   * Return the number of channel indices in the range.
+   */
   IndexType size() const;
 
+  /**
+   * Return the start value of the range.
+   */
   IndexType start() const { return mStart; }
 
+  /**
+   * Return the end value of the sequence.
+   * @note This is different from the last element of the sequence, because the class follows the 'one-past-the-end' semantics used in the STL and Python.
+   */
   IndexType end() const { return mEnd; }
 
+  /**
+   * Return the step size (stride) between consecutive elements.
+   */
   StepType step() const { return mStep; }
 
   /**
