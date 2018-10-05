589f79d64d1743243b46a395cf59b1ce4bba9f8f
Andreas Franck <a.franck@soton.ac.uk>
2018-10-05 09:46:35 +0100

    Added test case for using PYTHONPATH.

--- src/libpythoncomponents/test/wrapper.cpp
+++ src/libpythoncomponents/test/wrapper.cpp
@@ -17,6 +17,8 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/test/unit_test.hpp>
 
+#include <stdlib.h> // Use putenv() which is not in the C++ standard
+
 #include <stdexcept>
 #include <string>
 #include <sstream>
@@ -46,7 +48,7 @@ BOOST_AUTO_TEST_CASE( WrapSingleFileModule )
   boost::filesystem::path additionalPath{ PYTHON_MODULE_INSTALL_DIRECTORY };
 
   std::string const searchPath{ modulePath.string() + ", " + additionalPath.string() };
-
+  
   const std::size_t blockSize{64};
   const SamplingFrequencyType samplingFrequency{48000};
   std::size_t const numBlocks{16};
@@ -92,7 +94,7 @@ BOOST_AUTO_TEST_CASE( WrapSingleFileModule )
   }
 }
 
-// Wrap a Python component in a multi-file package)
+// Wrap a Python component contained in a multi-file package)
 BOOST_AUTO_TEST_CASE( WrapMultiFilePackage )
 {
   pythonsupport::InitialisationGuard::initialise();
@@ -119,7 +121,7 @@ BOOST_AUTO_TEST_CASE( WrapMultiFilePackage )
                   moduleName.c_str(),
                   "Adder",
                   "3,", "{'width':5}",
-                  searchPath.c_str() );
+                  modulePath.c_str() );
 
   std::stringstream errMsg;
   bool res = rrl::checkConnectionIntegrity( pyAtom1, true, errMsg );
@@ -152,6 +154,70 @@ BOOST_AUTO_TEST_CASE( WrapMultiFilePackage )
   }
 }
 
+// Wrap a Python component contained in a multi-file package)
+BOOST_AUTO_TEST_CASE( WrapUsePYTHONPATH )
+{
+  pythonsupport::InitialisationGuard::initialise();
+
+  std::string moduleName = "testmodule";
+
+  boost::filesystem::path basePath{CMAKE_CURRENT_SOURCE_DIR};
+  boost::filesystem::path const modulePath = basePath / "python";
+
+  // Use the path to the VISR python externals (retrieved from CMake)
+  boost::filesystem::path additionalPath{ PYTHON_MODULE_INSTALL_DIRECTORY };
+
+  std::string pythonPath = (std::string("PYTHONPATH=") + additionalPath.string());
+
+  // Pass the path to the VISR externals via the PYTHONPATH environment variable.
+  putenv( &pythonPath[0] );
+
+  const std::size_t blockSize{64};
+  const SamplingFrequencyType samplingFrequency{48000};
+  std::size_t const numBlocks{16};
+
+  SignalFlowContext ctxt( blockSize, samplingFrequency );
+
+  // Instantiate the atomic component (implemented in Python)
+  // by a mixture or poristional and keyword constructor arguments
+  Wrapper pyAtom1( ctxt, "PythonAtom", nullptr,
+                  moduleName.c_str(),
+                  "Adder",
+                  "3,", "{'width':5}",
+                  modulePath.c_str() );
+
+  std::stringstream errMsg;
+  bool res = rrl::checkConnectionIntegrity( pyAtom1, true, errMsg );
+  BOOST_CHECK_MESSAGE( res, errMsg.str() );
+
+  rrl::AudioSignalFlow flow( pyAtom1 );
+
+  std::size_t const numInputChannels = 15;
+  std::size_t const numOutputChannels = 5;
+
+  std::vector<SampleType*> inputPtr( numInputChannels, nullptr );
+  std::vector<SampleType*> outputPtr( numOutputChannels, nullptr );
+
+  efl::BasicMatrix<SampleType> inputData( numInputChannels, blockSize* numBlocks );
+  // TODO: Fill the input data with something useful
+
+  efl::BasicMatrix<SampleType> outputData( numOutputChannels, blockSize* numBlocks );
+
+  for( std::size_t blockIdx(0); blockIdx < numBlocks; ++blockIdx )
+  {
+    for( std::size_t idx(0); idx < numInputChannels; ++idx )
+    {
+      inputPtr[idx] = inputData.row(idx) + blockIdx*blockSize;
+    }
+    for( std::size_t idx(0); idx < numOutputChannels; ++idx )
+    {
+      outputPtr[idx] = outputData.row(idx) + blockIdx*blockSize;
+    }
+    flow.process( &inputPtr[0], &outputPtr[0] );
+  }
+}
+
+  
 } // namespace test
 } // namespace pythoncomponents
 } // namespace visr
