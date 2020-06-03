30a7959809d5a982d10a636000a87feac443a1e4
Andreas Franck <andreas.franck@audioscenic.com>
2020-06-03 09:41:31 +0100

    Overloaded Python __copy__ and __deepcopy__ methods.
    * Uses the polymorphic ParameterBase::assign() function.

--- src/python/visr/parameter_base.cpp
+++ src/python/visr/parameter_base.cpp
@@ -29,12 +29,32 @@ public:
    */
   using ParameterBase::ParameterBase;
 
+  virtual void assign(  ParameterBase const & rhs ) override
+  {
+    PYBIND11_OVERLOAD_PURE( void, ParameterBase, assign, rhs );
+  }
+
+  // TODO: Fix this overload. This is possibly related to the problems when returning a smart pointer from a pybind11 method.
+#if 0
+  virtual std::unique_ptr<ParameterBase> clone() const override
+  {
+    PYBIND11_OVERLOAD_PURE( std::unique_ptr<ParameterBase>, ParameterBase, clone, );
+  }
+#endif
 };
 
 void exportParameterBase( pybind11::module& m )
 {
-  pybind11::class_<ParameterBase/*, ParameterBaseWrapper*/>( m, "ParameterBase" )
+  pybind11::class_<ParameterBase, ParameterBaseWrapper>( m, "ParameterBase" )
     .def( "type", &ParameterBase::type )
+    .def( "assign", &ParameterBase::assign, pybind11::arg( "rhs" ) )
+    .def( "clone", &ParameterBase::clone )
+    .def( "__deepcopy__",
+    []( ParameterBase const & self, pybind11::dict & memo )
+    {
+      return self.clone();
+    } )
+    .def( "__copy__", &ParameterBase::clone )
     ;
 }
 
