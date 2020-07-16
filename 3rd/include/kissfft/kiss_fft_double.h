c4cbc31db3b55a15a69ac2000ae38909340bbaad
Andreas Franck <a.franck@soton.ac.uk>
2020-07-16 22:42:34 +0100

    Removed obsolete ffts and kissfft headers and binary libs.
    * Superseded by in-project compilation.

--- 3rd/include/kissfft/kiss_fft_double.h
+++
@@ -1,8 +0,0 @@
-#ifndef KISS_FFT_DOUBLE_H
-#define KISS_FFT_DOUBLE_H
-
-#include "kiss_fft_double_refedines.h"
-
-#include "kiss_fft.h"
-
-#endif // KISS_FFT_DOUBLE_H
--- 3rd/include/kissfft/kiss_fftr_float_redefines.h
+++
@@ -1,9 +0,0 @@
-// Copyright Andreas Franck - All rights reserved
-
-#define kiss_fft_scalar float
-
-#define kiss_fftr_state kiss_fftr_state_float
-#define kiss_fftr_cfg kiss_fftr_cfg_float
-#define kiss_fftr_alloc kiss_fftr_alloc_float
-#define kiss_fftr kiss_fftr_float
-#define kiss_fftri kiss_fftri_float
