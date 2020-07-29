Fixed check whether rotation update paremater input is present.

src/python/packages/visr_bst/hoa_components/hoa_rotation_matrix_calculator.py
@@ -96,7 +96,7 @@ class HoaRotationMatrixCalculator( visr.AtomicComponent ):
                                                       pml.EmptyParameterConfig() )
             self.orientationInputProtocol = self.orientationInput.protocolInput()
         else:
-            self.orientationInputProtocol is None
+            self.orientationInputProtocol = None
 
         if initialOrientation is None:
             initialOrientation = np.zeros( (3), np.float32 )