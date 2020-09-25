96c10de6a2fe6b03e9d9e9497caba5aec3ed2660
Andreas Franck <andreas.franck@audioscenic.com>
2020-09-25 16:22:06 +0100

    Switched loading of Python modules from C++ from deprecated 'imp' module to importlib.

--- src/libpythonsupport/load_module.cpp
+++ src/libpythonsupport/load_module.cpp
@@ -35,57 +35,23 @@ pybind11::object loadModule( std::string const & moduleName,
 {
   namespace py = pybind11;
 
-  // The python functions imp.find_module and imp.load_module does not support nested subpackages (with dots).
-  // This code follows https://docs.python.org/3/library/imp.html
-  std::vector<std::string> moduleNameParts;
-  boost::algorithm::split(moduleNameParts, moduleName, boost::algorithm::is_any_of("."));
-  if (moduleNameParts.empty())
-  {
-    throw std::invalid_argument( "pythonsupport::loadModule(): Invalid module name" );
-  }
-  // Remove whitespaces (because they are also ignored in Python)
-  std::for_each(moduleNameParts.begin(), moduleNameParts.end(),
-		[](std::string & v) { boost::algorithm::trim(v); });
-
   // Taken and adapted from
   // https://skebanga.github.io/embedded-python-pybind11/
   py::dict locals;
-  locals["moduleName"] = py::cast(moduleNameParts[0] );
+  locals["moduleName"] = py::cast( moduleName );
   locals["modulePath"] = modulePath.empty() ? py::none() : py::cast( modulePath );
   locals["additionalPath"] = additionalSystemPath.empty()
     ? py::list() : py::cast( additionalSystemPath );
   try
   {
-    // Adding the module path to the system path enables us to specify the path
-    // of other dependencies as well (e.g., the location of the VISR externals).
-    // We also use it as the path argument of imp.find_module to avoid finding
-    // other occurences on the system path.
     py::eval<py::eval_statements>( // tell eval we're passing multiple statements
-      "import imp\n"
+      "import importlib\n"
       "import sys\n"
       "sys.path += additionalPath\n"
-      "file, pathname, description = imp.find_module( moduleName, modulePath)\n"
-      "new_module = imp.load_module( moduleName, file, pathname, description)\n",
+      "new_module = importlib.import_module(moduleName, package=None)\n",
       globals,
       locals);
-
     py::object mod = locals["new_module"];
-    // Recursively iterate through nested sub-package names (if there are any)
-    for (std::size_t nestingLevel(1); nestingLevel < moduleNameParts.size();
-	 ++nestingLevel)
-    {
-      py::object newModulePath = mod.attr("__path__");
-      locals["moduleName"] = py::cast(moduleNameParts[nestingLevel].c_str());
-      locals["modulePath"] = newModulePath;
-      py::eval<py::eval_statements>( // tell eval we're passing multiple statements
-	"sys.path += additionalPath\n"
-	"file, pathname, description = imp.find_module( moduleName, modulePath)\n"
-	"new_module = imp.load_module( moduleName, file, pathname, description)\n",
-	globals,
-	locals);
-
-      mod = locals["new_module"];
-    }
     return mod;
   }
   catch( std::exception const & ex )
