s3a-spatialaudio/VISR

commit a60ae0f32f07ee0fb7df8cfcbedae49b7a1ae67e

Conditional code for rpath adjustment and 3rd-party lbrary installation.
* Handle build options BUILD_USE_SNDFILE_LIBRARY and BUILD_AUDIOINTERFACES_PORTAUDIO.

cmake_modules/Adjust3rdPartyLibraries.cmake
@@ -67,12 +67,19 @@ file( MAKE_DIRECTORY ${VISR_BUILD_3RD_PARTY_RUNTIME_LIBRARY_DIR} )
 
 # List of 3rd party libraries that are not boost.
 # More specifically, these are not imported targets as preferred in modern CMake.
-set( FIX_LIBRARIES SNDFILE_LIBRARY PORTAUDIO_LIBRARIES)
+set( FIX_LIBRARIES )
 
-# On MacOS, sndfile depends on FLAC, OGG, and Vorbis.
-if( VISR_SYSTEM_NAME MATCHES "MacOS" )
- list( APPEND FIX_LIBRARIES FLAC_LIBRARY OGG_LIBRARY VORBIS_LIBRARY VORBISENC_LIBRARY )
-endif( VISR_SYSTEM_NAME MATCHES "MacOS" )
+if( BUILD_AUDIOINTERFACES_PORTAUDIO )
+  list( APPEND FIX_LIBRRIES PORTAUDIO_LIBRARIES )
+endif( BUILD_AUDIOINTERFACES_PORTAUDIO )
+
+if( BUILD_USE_SNDFILE_LIBRARY )
+  list( APPEND FIX_LIBRRIES SNDFILE_LIBRARY )
+  # On MacOS, sndfile depends on FLAC, OGG, and Vorbis.
+  if( VISR_SYSTEM_NAME MATCHES "MacOS" )
+   list( APPEND FIX_LIBRARIES FLAC_LIBRARY OGG_LIBRARY VORBIS_LIBRARY VORBISENC_LIBRARY )
+  endif( VISR_SYSTEM_NAME MATCHES "MacOS" )
+endif( BUILD_USE_SNDFILE_LIBRARY )
 
 # On Mac OS, the Python library must also be treated because of the rpath.
 if( BUILD_PYTHON_BINDINGS AND (VISR_SYSTEM_NAME MATCHES "MacOS") )
@@ -102,17 +109,20 @@ if(VISR_SYSTEM_NAME MATCHES "Windows")
       copyDllFromTarget( Boost::${BOOST_LIBRARY} ${VISR_BUILD_3RD_PARTY_RUNTIME_LIBRARY_DIR} )
     endforeach()
   endif( NOT BOOST_USE_STATIC_LIBS )
-  # special treatment for the PortAudio DLL, because its DLL has a non-matching file name.
-  get_filename_component( PORTAUDIO_LIBRARY_DIR ${PORTAUDIO_LIBRARIES} DIRECTORY )
-  get_filename_component( PORTAUDIO_LIBRARY_DIR ${PORTAUDIO_LIBRARY_DIR} REALPATH )
-  set( PORTAUDIO_DLL_NAME ${PORTAUDIO_LIBRARY_DIR}/portaudio_x64.dll )
-  copyLibrary( ${PORTAUDIO_DLL_NAME} ${VISR_BUILD_3RD_PARTY_RUNTIME_LIBRARY_DIR})
-  # Same for sndfile, because the DLL name differs from the name of the import lib.
-  get_filename_component( SNDFILE_LIBRARY_DIR ${SNDFILE_LIBRARIES} DIRECTORY )
-  get_filename_component( SNDFILE_LIBRARY_DIR ${SNDFILE_LIBRARY_DIR} REALPATH )
-  set( SNDFILE_DLL_NAME ${SNDFILE_LIBRARY_DIR}/libsndfile-1.dll )
-  copyLibrary( ${SNDFILE_DLL_NAME} ${VISR_BUILD_3RD_PARTY_RUNTIME_LIBRARY_DIR})
-
+  if( BUILD_AUDIOINTERFACES_PORTAUDIO )
+    # special treatment for the PortAudio DLL, because its DLL has a non-matching file name.
+    get_filename_component( PORTAUDIO_LIBRARY_DIR ${PORTAUDIO_LIBRARIES} DIRECTORY )
+    get_filename_component( PORTAUDIO_LIBRARY_DIR ${PORTAUDIO_LIBRARY_DIR} REALPATH )
+    set( PORTAUDIO_DLL_NAME ${PORTAUDIO_LIBRARY_DIR}/portaudio_x64.dll )
+    copyLibrary( ${PORTAUDIO_DLL_NAME} ${VISR_BUILD_3RD_PARTY_RUNTIME_LIBRARY_DIR})
+  endif( BUILD_AUDIOINTERFACES_PORTAUDIO )
+  if( BUILD_USE_SNDFILE_LIBRARY )
+    # Same for sndfile, because the DLL name differs from the name of the import lib.
+    get_filename_component( SNDFILE_LIBRARY_DIR ${SNDFILE_LIBRARIES} DIRECTORY )
+    get_filename_component( SNDFILE_LIBRARY_DIR ${SNDFILE_LIBRARY_DIR} REALPATH )
+    set( SNDFILE_DLL_NAME ${SNDFILE_LIBRARY_DIR}/libsndfile-1.dll )
+    copyLibrary( ${SNDFILE_DLL_NAME} ${VISR_BUILD_3RD_PARTY_RUNTIME_LIBRARY_DIR})
+  endif( BUILD_USE_SNDFILE_LIBRARY )
 endif(VISR_SYSTEM_NAME MATCHES "Windows")
 
 ################################################################################

cmake_modules/PackageConfig.cmake
@@ -29,11 +29,14 @@ if( WIN32 )
   set( CPACK_GENERATOR NSIS )
   set( CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
   set( CPACK_PACKAGE_INSTALL_DIRECTORY ${VISR_VERSIONED_NAME} )
-  get_filename_component( PORTAUDIO_LIBRARY_DIR ${PORTAUDIO_LIBRARY} DIRECTORY )
-  install( FILES ${PORTAUDIO_LIBRARY_DIR}/portaudio_x64.dll DESTINATION 3rd COMPONENT thirdparty_libraries )
-
-  get_filename_component( SNDFILE_LIBRARY_DIR ${SNDFILE_LIBRARY} DIRECTORY )
-  install( FILES ${SNDFILE_LIBRARY_DIR}/libsndfile-1.dll DESTINATION 3rd COMPONENT thirdparty_libraries )
+  if( BUILD_AUDIOINTERFACES_PORTAUDIO )
+    get_filename_component( PORTAUDIO_LIBRARY_DIR ${PORTAUDIO_LIBRARY} DIRECTORY )
+    install( FILES ${PORTAUDIO_LIBRARY_DIR}/portaudio_x64.dll DESTINATION 3rd COMPONENT thirdparty_libraries )
+  endif( BUILD_AUDIOINTERFACES_PORTAUDIO )
+  if( BUILD_USE_SNDFILE_LIBRARY )
+    get_filename_component( SNDFILE_LIBRARY_DIR ${SNDFILE_LIBRARY} DIRECTORY )
+    install( FILES ${SNDFILE_LIBRARY_DIR}/libsndfile-1.dll DESTINATION 3rd COMPONENT thirdparty_libraries )
+  endif( BUILD_USE_SNDFILE_LIBRARY )
 
   # Boost
   if( NOT Boost_USE_STATIC_LIBS )
@@ -61,13 +64,16 @@ if( VISR_SYSTEM_NAME MATCHES "MacOS" )
 
   set( CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" )
 
-  install( FILES ${PORTAUDIO_LIBRARIES} DESTINATION 3rd COMPONENT thirdparty_libraries)
-  install( FILES ${SNDFILE_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
-  install( FILES ${FLAC_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
-  install( FILES ${OGG_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
-  install( FILES ${VORBIS_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
-  install( FILES ${VORBISENC_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
-
+  if( BUILD_AUDIOINTERFACES_PORTAUDIO )
+    install( FILES ${PORTAUDIO_LIBRARIES} DESTINATION 3rd COMPONENT thirdparty_libraries)
+  endif( BUILD_AUDIOINTERFACES_PORTAUDIO )
+  if( BUILD_USE_SNDFILE_LIBRARY )
+    install( FILES ${SNDFILE_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
+    install( FILES ${FLAC_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
+    install( FILES ${OGG_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
+    install( FILES ${VORBIS_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
+    install( FILES ${VORBISENC_LIBRARY} DESTINATION 3rd COMPONENT thirdparty_libraries)
+  endif( BUILD_USE_SNDFILE_LIBRARY )
   if( NOT Boost_USE_STATIC_LIBS )
     foreach( BOOSTLIB ${VISR_BOOST_INSTALL_LIBRARIES} )
       get_target_property( BOOSTLIBPATH Boost::${BOOSTLIB} IMPORTED_LOCATION )

commit 15464c5471ca4b357a5e0701d4f9f204b5fa01e6

fix typo in 3rd party library adjustment
introduced in a60ae0f32f07ee0fb7df8cfcbedae49b7a1ae67e
partially fixed in beca6466220921d473b04ca9caac0e8638fb33e2

cmake_modules/Adjust3rdPartyLibraries.cmake
@@ -70,7 +70,7 @@ file( MAKE_DIRECTORY ${VISR_BUILD_3RD_PARTY_RUNTIME_LIBRARY_DIR} )
 set( FIX_LIBRARIES )
 
 if( BUILD_AUDIOINTERFACES_PORTAUDIO )
-  list( APPEND FIX_LIBRRIES PORTAUDIO_LIBRARIES )
+  list( APPEND FIX_LIBRARIES PORTAUDIO_LIBRARIES )
 endif( BUILD_AUDIOINTERFACES_PORTAUDIO )
 
 if( BUILD_USE_SNDFILE_LIBRARY )