a51a6f721021d9348e67b772766c9e4acd32719c
Andreas Franck <andreas.franck@audioscenic.com>
2023-03-05 16:37:22 +0000

    Add setup.py because pyproject.toml and setup.cgf apparently do not support ext_modules.

---
+++ src/python/package/setup.py
@@ -0,0 +1,8 @@
+# Minimal setup.py scrupt for the parts that cannot be expressed via pyproject.toml and setup.cfg
+
+from setuptools import setup, Distribution
+from distutils.core import Extension
+
+setup(
+  ext_modules = [ Extension(name="_", sources=[]) ]
+)
