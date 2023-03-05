3a6794c44972365d8e4d091a2cefe15f8b1269f2
Andreas Franck <andreas.franck@audioscenic.com>
2023-03-05 16:34:29 +0000

    Add all other VISR packages (except visr core) as sub-packages.

--- src/python/package/src/__init__.py
+++ src/python/package/src/__init__.py
@@ -18,21 +18,12 @@ from contextlib import contextmanager
 # with dll_path_context(os.path.join(os.getcwd(), 'impl')):
 #     from .impl.visr import *
 
-cookie = os.add_dll_directory(
-   os.path.join(os.path.dirname(os.path.realpath(__file__)), 'impl'))
+if hasattr(os, 'add_dll_directory'):
+   cookie = os.add_dll_directory(
+      os.path.join(os.path.dirname(os.path.realpath(__file__)), 'impl'))
+else:
+   cookie = None
 from .impl.visr import *
-# from .impl.audiointerfaces import audiointerfaces
 
-from .impl.efl import efl
-# from .impl.efl import efl
-# from .impl.objectmodel import objectmodel
-# from .impl.panning import panning
-# from .impl.panningdsp import panningdsp
-# from .impl.pml import pml
-# from .impl.rbbl import rbbl
-# from .impl.reverbobject import reverbobject
-# from .impl.rcl import rcl
-# from .impl.rrl import rrl
-# from .impl.signalflows import signalflows
-
-cookie.close()
+if cookie is not None:
+   cookie.close()
---
+++ src/python/package/src/panning/__init__.py
@@ -0,0 +1,12 @@
+import os
+from contextlib import contextmanager
+
+if hasattr(os, 'add_dll_directory'):
+   cookie = os.add_dll_directory(
+      os.path.join(os.path.dirname(os.path.realpath(__file__)), 'impl'))
+else:
+   cookie = None
+from ..impl.panning import *
+
+if cookie is not None:
+   cookie.close()
