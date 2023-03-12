8bb42daa4545aad3252e050d462b6a8a4604c93e
Andreas Franck <andreas.franck@audioscenic.com>
2023-03-12 18:51:27 +0000

    Create namespaced package structure during build of Python externals.

--- src/python/package/src/audiointerfaces/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-from ..impl.dll_path_context import dll_path_context
-
-
-with dll_path_context(
-  os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'impl')):
-    from ..impl.audiointerfaces import *
--- src/python/package/src/impl/__init__.py
+++
@@ -1,2 +0,0 @@
-# Intentionally empty __init__.py file, required to be discovered by the
-# binary dist generation.
