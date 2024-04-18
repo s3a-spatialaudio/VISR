fd4c2c9daa994045e458ecf0c10d0c9a3467bd06
Thomas Nixon <tom@tomn.co.uk>
2024-04-18 19:57:35 +0100

    avoid deleting python sample type objects without GIL
    The sample type lookup table holds references to python objects, and is
    constructed statically. This is dangerous during initialisation (as the
    python interpreter is not yet running, but actually causes problems at
    finalisation.
    
    If the python interpreter gets shut down first (which can happen as it's
    static too), a pybind11 assertion fires when it's finalised, as the GIL
    is not held.
    
    Use reference counting from the callback lambdas instead.

--- src/python/visr/audio_sample_type.cpp
+++ src/python/visr/audio_sample_type.cpp
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <ciso646>
 #include <cstdint>
+#include <memory>
 #include <utility>
 #include <vector>
 
@@ -29,24 +30,33 @@ namespace // unnamed
 using AudioSampleTypeLookupEntry = std::tuple<py::dtype, AudioSampleType::Id>;
 using AudioSampleTypeLookup = std::vector< AudioSampleTypeLookupEntry >;
 
-static const  AudioSampleTypeLookup sSampleTypeIdLookup = {
-    AudioSampleTypeLookupEntry{ py::dtype::of<float>(), AudioSampleType::floatId },
-    AudioSampleTypeLookupEntry{ py::dtype::of<double>(), AudioSampleType::doubleId },
-    AudioSampleTypeLookupEntry{ py::dtype::of<long double>(), AudioSampleType::longDoubleId },
-    AudioSampleTypeLookupEntry{ py::dtype::of<uint8_t>(), AudioSampleType::uint8Id },
-    AudioSampleTypeLookupEntry{ py::dtype::of<int8_t>(), AudioSampleType::int8Id },
-    AudioSampleTypeLookupEntry{ py::dtype::of<int16_t>(), AudioSampleType::uint16Id },
-    AudioSampleTypeLookupEntry{ py::dtype::of<int16_t>(), AudioSampleType::uint16Id },
-    AudioSampleTypeLookupEntry{ py::dtype::of<uint32_t>(), AudioSampleType::uint32Id },
-    AudioSampleTypeLookupEntry{ py::dtype::of<int32_t>(), AudioSampleType::int32Id },
-    AudioSampleTypeLookupEntry{ py::dtype::of< std::complex<float> >(), AudioSampleType::complexFloatId },
-    AudioSampleTypeLookupEntry{ py::dtype::of< std::complex<double> >(), AudioSampleType::complexDoubleId }
-};
+static AudioSampleTypeLookup getSampleTypeIdLookup() {
+  AudioSampleTypeLookup table = {
+      AudioSampleTypeLookupEntry{ py::dtype::of<float>(), AudioSampleType::floatId },
+      AudioSampleTypeLookupEntry{ py::dtype::of<double>(), AudioSampleType::doubleId },
+      AudioSampleTypeLookupEntry{ py::dtype::of<long double>(), AudioSampleType::longDoubleId },
+      AudioSampleTypeLookupEntry{ py::dtype::of<uint8_t>(), AudioSampleType::uint8Id },
+      AudioSampleTypeLookupEntry{ py::dtype::of<int8_t>(), AudioSampleType::int8Id },
+      AudioSampleTypeLookupEntry{ py::dtype::of<int16_t>(), AudioSampleType::uint16Id },
+      AudioSampleTypeLookupEntry{ py::dtype::of<int16_t>(), AudioSampleType::uint16Id },
+      AudioSampleTypeLookupEntry{ py::dtype::of<uint32_t>(), AudioSampleType::uint32Id },
+      AudioSampleTypeLookupEntry{ py::dtype::of<int32_t>(), AudioSampleType::int32Id },
+      AudioSampleTypeLookupEntry{ py::dtype::of< std::complex<float> >(), AudioSampleType::complexFloatId },
+      AudioSampleTypeLookupEntry{ py::dtype::of< std::complex<double> >(), AudioSampleType::complexDoubleId }
+  };
+
+  return table;
+}
 
 } // unnamed namespace
 
 void exportAudioSampleType( py::module & m )
 {
+  // shared pointer is used here so that the references to python objects that
+  // this holds are destroyed when the module is destroyed, not at program exit
+  // where the GIL is not held
+  auto sampleTypeIdLookup = std::make_shared<AudioSampleTypeLookup>(getSampleTypeIdLookup());
+
   py::enum_<AudioSampleType::Id>( m, "AudioSampleType", "Enumeration of type ids for the supported audio sample types." )
     .value( "floatId", AudioSampleType::floatId )
     .value( "doubleId", AudioSampleType::doubleId )
@@ -62,25 +72,25 @@ void exportAudioSampleType( py::module & m )
     ;
 
   m.def( "audioSampleTypes",
-    []()
+    [sampleTypeIdLookup]()
     {
       std::vector<AudioSampleType::Id> sampleTypes;
-      sampleTypes.reserve( sSampleTypeIdLookup.size() );
-      std::transform( sSampleTypeIdLookup.begin(), sSampleTypeIdLookup.end(), std::back_inserter( sampleTypes),
+      sampleTypes.reserve( sampleTypeIdLookup->size() );
+      std::transform( sampleTypeIdLookup->begin(), sampleTypeIdLookup->end(), std::back_inserter( sampleTypes),
         []( AudioSampleTypeLookup::value_type const & v ) { return std::get<1>( v ); } );
       return sampleTypes;
     },
     "Return a list of registered audio sample types."
   );
 
-  m.def( "audioSampleTypeToDtype", []( AudioSampleType::Id sampleType )
+  m.def( "audioSampleTypeToDtype", [sampleTypeIdLookup]( AudioSampleType::Id sampleType )
   {
-    auto const findIt = std::find_if( sSampleTypeIdLookup.begin(), sSampleTypeIdLookup.end(),
+    auto const findIt = std::find_if( sampleTypeIdLookup->begin(), sampleTypeIdLookup->end(),
       [sampleType]( AudioSampleTypeLookup::value_type const & v ) -> bool
       {
         return std::get<1>( v ) == sampleType;
       } );
-    if( findIt == sSampleTypeIdLookup.end() )
+    if( findIt == sampleTypeIdLookup->end() )
     {
       throw std::out_of_range( "Audio sample type does not match a Python data type." );
     }
@@ -89,14 +99,14 @@ void exportAudioSampleType( py::module & m )
   "Convert a VISR audio sample type into the corresponding Numpy dtype."
   );
 
-  m.def( "dtypeToAudioSampleType", []( py::dtype pyType )
+  m.def( "dtypeToAudioSampleType", [sampleTypeIdLookup]( py::dtype pyType )
     {
-      auto const findIt = std::find_if( sSampleTypeIdLookup.begin(), sSampleTypeIdLookup.end(), 
+      auto const findIt = std::find_if( sampleTypeIdLookup->begin(), sampleTypeIdLookup->end(),
         [pyType]( AudioSampleTypeLookup::value_type const & v ) -> bool
         {
           return pyType.is(std::get<0>( v ));
         } );
-      if( findIt == sSampleTypeIdLookup.end() )
+      if( findIt == sampleTypeIdLookup->end() )
       {
         throw std::out_of_range( "Python dtype does not match a registered audio sample type." );
       }
